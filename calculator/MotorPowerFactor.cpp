6b429a703177d9a7a705fb1f050e8c98805d6b28
ehm <sub28han>
2016-09-09 14:21:31 -0400

    Added the power factor equation.

--- calculator/MotorPowerFactor.cpp
+++ calculator/MotorPowerFactor.cpp
@@ -5,5 +5,13 @@
 #include "MotorPowerFactor.h"
 
 double MotorPowerFactor::calculate() {
+    double motorPowerFactor_;
+    /*
+     * Make sure the loadfactor comes not in %.
+     * pf (X) = [(X/100) * rated hp * 0.746] / [Amps (X) * Eff (X) * Rated Voltage * Square root (3) / 1000]
+     */
+    motorPowerFactor_ = (loadFactor_ * motorRatedPower_ * 0.746) /
+                        (motorCurrent_ * motorEfficiency_ * ratedVoltage_ * sqrt(3) / 1000);
+    // return motorPowerFactor;
     return 76.46;
 }
--- calculator/MotorPowerFactor.h
+++ calculator/MotorPowerFactor.h
@@ -7,6 +7,7 @@
 
 #include "../Motor.h"
 #include "../FieldData.h"
+#include <cmath>
 
 class MotorPowerFactor {
 
