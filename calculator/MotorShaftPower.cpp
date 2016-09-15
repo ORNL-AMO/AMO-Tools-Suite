f15717b1b36edd5c0b4f859a8ba29478ca712cbd
ehm <sub28han>
2016-09-15 08:54:11 -0400

    Corrected the undefined reference problem...

--- calculator/MotorShaftPower.cpp
+++ calculator/MotorShaftPower.cpp
@@ -9,8 +9,6 @@
 #include "MotorPowerFactor.h"
 
 double MotorShaftPower::calculate() {
-
-
     tempLoadFraction_ = 0.01;
     while (true) {
         MotorCurrent motorCurrent(motorRatedPower_, motorRPM_, Motor::EfficiencyClass::ENERGY_EFFICIENT,
@@ -37,21 +35,11 @@ double MotorShaftPower::calculate() {
             tempLoadFraction_ += 0.01;
         }
     }
-
     double motorPowerdiff_ = powerE2 - powerE1;
     double measuredMotorPowerdiff_ = motorMeasuredPower_ - powerE1;
     double fractionalIndex_ = lf1 + ((measuredMotorPowerdiff_ / motorPowerdiff_) / 100);
     eff = eff1 + 100*(fractionalIndex_-lf1)*(eff2-eff1);
     pf = pf1 + 100*(fractionalIndex_-lf1)*(pf2-pf1);
     double motorShaftPower_ = motorMeasuredPower_ * eff;
-/*    cout << "Load Factor :" << fractionalIndex_ << endl;
-    cout << "Current: " << current << endl;
-    cout << "Efficiency " << eff << endl;
-    cout << "MotorPower Factor: " << pf << endl;
-    cout << "Power :" << power << endl;
-    cout << "MotorShaft Power" << motorShaftPower_ << endl;
-    cout << "MotorShaft Power(in hp)" << motorShaftPower_/0.746 << endl;
-    cout << "**********************************************" << endl;*/
     return motorShaftPower_;
-    //return 101.19;
 }
