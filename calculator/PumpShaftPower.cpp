ff2f614c3aec0da8cbe0432d28632605b584ccce
ehm <mishras@ornl.gov>
2016-10-06 13:16:46 -0400

    - Belt Drive calculation

--- calculator/PumpShaftPower.cpp
+++ calculator/PumpShaftPower.cpp
@@ -12,7 +12,13 @@ double PumpShaftPower::calculate() {
     if (drive_ == Pump::Drive::DIRECT_DRIVE) {
         pumpShaftPower_ = motorShaftPower_;
     } else {
-        // Calculate using the sheet
+        // Belt Drive scenario
+        double BLM = motorShaftPower_ * ((0.68759 * exp((motorShaftPower_ / 0.746) * (-0.019791)) +
+                                          3.7558 * exp((motorShaftPower_ / 0.746) * (-0.21507)) + 3.9963) / 100);
+        // motorShaftPower_ is already in hp , so make BLM in hp
+        BLM = BLM / 0.746;
+        double motorSheavePowerRatio = motorShaftPower_ / (motorShaftPower_ - BLM);
+        pumpShaftPower_ = motorShaftPower_ - BLM;
     }
     return pumpShaftPower_;
 }
