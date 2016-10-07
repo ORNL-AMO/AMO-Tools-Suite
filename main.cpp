7a50658deb741a30cf11d513a060f5e2cd2e49b6
ehm <mishras@ornl.gov>
2016-10-07 16:15:14 -0400

    For load estimation :: Current case, with fieldPower = 0

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ int main() {
     Pump pump(Pump::Style::API_DOUBLE_SUCTION, 1780, Pump::Drive::DIRECT_DRIVE, 1.00, 1.000, 1, Pump::Speed::NOT_FIXED_SPEED);
     Motor motor(Motor::LineFrequency::FREQ60, 200, 1780, Motor::EfficiencyClass::ENERGY_EFFICIENT, 95, 460, 225.0, 0);
     Financial financial(1.000, 0.05);
-    FieldData fieldData(1840, 174.85, FieldData::LoadEstimationMethod::CURRENT, 0, 125.857, 480);
+    FieldData fieldData(2000, 277, FieldData::LoadEstimationMethod::POWER, 80, 0, 480);
     //FieldData fieldData1(1840, 174.85, FieldData::LoadEstimationMethod::POWER, 80, 125.857, 480);
+
     PSATResult psatResult(pump,motor,financial,fieldData);
     psatResult.calculateExisting();
     cout << "Pump Efficiency: " << psatResult.getExisting().pumpEfficiency_ << endl;
@@ -22,4 +23,19 @@ int main() {
     cout << "Annual Energy: " << psatResult.getExisting().annualEnergy_ << endl;
     cout << "Annual Cost: " << psatResult.getExisting().annualCost_ << endl;
     cout << "Estimated FLA: " << psatResult.getExisting().estimatedFLA_ << endl;
+
+
+    Pump pump1(Pump::Style::END_SUCTION_ANSI_API,1780,Pump::Drive::DIRECT_DRIVE,
+              1,1,1,Pump::Speed::NOT_FIXED_SPEED);
+    Motor motor1(Motor::LineFrequency::FREQ60,200,1786,
+                Motor::EfficiencyClass::ENERGY_EFFICIENT,0,460,225.8,0);
+    Financial fin(1,.05);
+    FieldData fd(2000,277,FieldData::LoadEstimationMethod::POWER,
+                 80,0,460);
+
+    PSATResult psat(pump1,motor1,fin,fd);
+    psat.calculateExisting();
+    auto ex = psat.getExisting();
+
+    cout << "msp " << ex.motorShaftPower_ << endl;
 }
