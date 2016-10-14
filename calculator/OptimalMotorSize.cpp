aab235fba3e7d0b3b34a9a4b7235a721daea798c
ehm <mishras@ornl.gov>
2016-10-14 09:33:07 -0400

    Update.

---
+++ calculator/OptimalMotorSize.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mishra, Subhankar on 10/14/16.
+//
+
+#include "OptimalMotorSize.h"
---
+++ calculator/OptimalMotorSize.h
@@ -0,0 +1,14 @@
+//
+// Created by Mishra, Subhankar on 10/14/16.
+//
+
+#ifndef AMO_LIBRARY_OPTIMALMOTORSIZE_H
+#define AMO_LIBRARY_OPTIMALMOTORSIZE_H
+
+
+class OptimalMotorSize {
+
+};
+
+
+#endif //AMO_LIBRARY_OPTIMALMOTORSIZE_H
