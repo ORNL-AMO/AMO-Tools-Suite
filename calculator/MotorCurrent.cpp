9215f34e698c617522228fb21ab419a2f3422ffe
ehm <mishras@ornl.gov>
2016-10-31 09:21:25 -0400

    Issue 7

--- calculator/MotorCurrent.cpp
+++ calculator/MotorCurrent.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "MotorCurrent.h"
+#include <iostream>
+using namespace std;
 
 double MotorCurrent::calculate() {
 
@@ -26,6 +28,7 @@ double MotorCurrent::calculate() {
     // Adjustment based on the rated voltage.
     for (int i = 0; i < 6; i++) {
         plValues[i] = plValues[i] * 460 / ratedVoltage_;
+
     }
 
     // Adjustment based on the specified FLA
@@ -33,17 +36,18 @@ double MotorCurrent::calculate() {
     for (int i = 0; i < 6; i++) {
         plValues[i] = plValues[i]*fullLoadAmps_/temp_fullLoadAmps;
     }
-    if (loadFactor_ < 0.25) {
+    if (loadFactor_ < 0.25 || loadFactor_ == 0.25 || std::abs(loadFactor_- 0.25) < 0.001) {
             double xCoord_[3] = {0, .25, .50};
             double yCoord_[3] = {plValues[0], plValues[1], plValues[2]};
             CurveFitVal cfv(3, xCoord_, yCoord_, 2, loadFactor_);
             motorCurrent_ = cfv.calculate();
-        } else if (loadFactor_ > 0.25 && loadFactor_ <= 1.25) {
+
+        } else if (loadFactor_ > 0.25 && (loadFactor_ < 1.25 || loadFactor_ == 1.25 || std::abs(loadFactor_- 1.25) < 0.001 )) {
             double xCoord_[5] = {.25, .50, .75, 1.00, 1.25};
             double yCoord_[5] = {plValues[1], plValues[2], plValues[3], plValues[4], plValues[5]};
             CurveFitVal cfv(5, xCoord_, yCoord_, 4, loadFactor_);
             motorCurrent_ = cfv.calculate();
-    } else if (loadFactor_ > 1.25 && loadFactor_ <= 1.50) {
+    } else if (loadFactor_ > 1.25 && (loadFactor_ < 1.50) || loadFactor_ == 1.50 || std::abs(loadFactor_- 1.50) < 0.001) {
         double xCoord_[3] = {.75, 1.00, 1.25};
         double yCoord_[3] = {plValues[3], plValues[4], plValues[5]};
         CurveFitVal cfv(3, xCoord_, yCoord_, 2, loadFactor_);
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "PSATResult.h"
+#include "./calculator/MotorCurrent.h"
 #include <iomanip>
 using namespace std;
 
@@ -7,40 +8,48 @@ int main() {
 
     std::cout << "Hello, World!" << std::endl;
     //cout <<  setprecision(16);
-    Pump pump(Pump::Style::END_SUCTION_ANSI_API, 90, 1780, Pump::Drive::DIRECT_DRIVE, 1.00, 1.000, 2, Pump::Speed::NOT_FIXED_SPEED);
-    Motor motor(Motor::LineFrequency::FREQ60, 200, 1780, Motor::EfficiencyClass::SPECIFIED, 95, 460, 225.0, 0);
-    Financial financial(1.000, 0.05);
-    FieldData fieldData(1840, 174.85, FieldData::LoadEstimationMethod::POWER, 80, 125.857, 480);
+//    Pump pump(Pump::Style::END_SUCTION_ANSI_API, 90, 1780, Pump::Drive::DIRECT_DRIVE, 1.00, 1.000, 2, Pump::Speed::NOT_FIXED_SPEED);
+//    Motor motor(Motor::LineFrequency::FREQ60, 200, 1780, Motor::EfficiencyClass::SPECIFIED, 95, 460, 225.0, 0);
+//    Financial financial(1.000, 0.05);
+//    FieldData fieldData(1840, 174.85, FieldData::LoadEstimationMethod::POWER, 80, 125.857, 480);
+//
+//    PSATResult psatResult(pump,motor,financial,fieldData);
+//    psatResult.calculateExisting();
+//    psatResult.calculateOptimal();
+//
+//    cout << "Pump Efficiency: " << psatResult.getExisting().pumpEfficiency_ << endl;
+//    cout << "Motor Rated power: " << psatResult.getExisting().motorRatedPower_ << endl;
+//    cout << "Motor Shaft power: " << psatResult.getExisting().motorShaftPower_ << endl;
+//    cout << "Pump Shaft power: " << psatResult.getExisting().pumpShaftPower_ << endl;
+//    cout << "Motor Efficiency: " << psatResult.getExisting().motorEfficiency_ << endl;
+//    cout << "Motor power factor: " << psatResult.getExisting().motorPowerFactor_ << endl;
+//    cout << "Motor current: " << psatResult.getExisting().motorCurrent_ << endl;
+//    cout << "Motor power: " << psatResult.getExisting().motorPower_ << endl;
+//    cout << "Annual Energy: " << psatResult.getExisting().annualEnergy_ << endl;
+//    cout << "Annual Cost: " << psatResult.getExisting().annualCost_ << endl;
+//    cout << "Estimated FLA: " << psatResult.getExisting().estimatedFLA_ << endl;
+//
+//    cout << "Optimal **********************************" << endl;
+//    cout << "Pump Efficiency: " << psatResult.getOptimal().pumpEfficiency_ << endl;
+//    cout << "Motor Rated power: " << psatResult.getOptimal().motorRatedPower_ << endl;
+//    cout << "Motor Shaft power: " << psatResult.getOptimal().motorShaftPower_ << endl;
+//    cout << "Pump Shaft power: " << psatResult.getOptimal().pumpShaftPower_ << endl;
+//    cout << "Motor Efficiency: " << psatResult.getOptimal().motorEfficiency_ << endl;
+//    cout << "Motor power factor: " << psatResult.getOptimal().motorPowerFactor_ << endl;
+//    cout << "Motor current: " << psatResult.getOptimal().motorCurrent_ << endl;
+//    cout << "Motor power: " << psatResult.getOptimal().motorPower_ << endl;
+//    cout << "Annual Energy: " << psatResult.getOptimal().annualEnergy_ << endl;
+//    cout << "Annual Cost: " << psatResult.getOptimal().annualCost_ << endl;
+//
+//    cout << "getAnnualSavingsPotential: " << psatResult.getAnnualSavingsPotential() << endl;
+//    cout << "getOptimizationRating: " << psatResult.getOptimizationRating() << endl;
 
-    PSATResult psatResult(pump,motor,financial,fieldData);
-    psatResult.calculateExisting();
-    psatResult.calculateOptimal();
-
-    cout << "Pump Efficiency: " << psatResult.getExisting().pumpEfficiency_ << endl;
-    cout << "Motor Rated power: " << psatResult.getExisting().motorRatedPower_ << endl;
-    cout << "Motor Shaft power: " << psatResult.getExisting().motorShaftPower_ << endl;
-    cout << "Pump Shaft power: " << psatResult.getExisting().pumpShaftPower_ << endl;
-    cout << "Motor Efficiency: " << psatResult.getExisting().motorEfficiency_ << endl;
-    cout << "Motor power factor: " << psatResult.getExisting().motorPowerFactor_ << endl;
-    cout << "Motor current: " << psatResult.getExisting().motorCurrent_ << endl;
-    cout << "Motor power: " << psatResult.getExisting().motorPower_ << endl;
-    cout << "Annual Energy: " << psatResult.getExisting().annualEnergy_ << endl;
-    cout << "Annual Cost: " << psatResult.getExisting().annualCost_ << endl;
-    cout << "Estimated FLA: " << psatResult.getExisting().estimatedFLA_ << endl;
-
-    cout << "Optimal **********************************" << endl;
-    cout << "Pump Efficiency: " << psatResult.getOptimal().pumpEfficiency_ << endl;
-    cout << "Motor Rated power: " << psatResult.getOptimal().motorRatedPower_ << endl;
-    cout << "Motor Shaft power: " << psatResult.getOptimal().motorShaftPower_ << endl;
-    cout << "Pump Shaft power: " << psatResult.getOptimal().pumpShaftPower_ << endl;
-    cout << "Motor Efficiency: " << psatResult.getOptimal().motorEfficiency_ << endl;
-    cout << "Motor power factor: " << psatResult.getOptimal().motorPowerFactor_ << endl;
-    cout << "Motor current: " << psatResult.getOptimal().motorCurrent_ << endl;
-    cout << "Motor power: " << psatResult.getOptimal().motorPower_ << endl;
-    cout << "Annual Energy: " << psatResult.getOptimal().annualEnergy_ << endl;
-    cout << "Annual Cost: " << psatResult.getOptimal().annualCost_ << endl;
-
-    cout << "getAnnualSavingsPotential: " << psatResult.getAnnualSavingsPotential() << endl;
-    cout << "getOptimizationRating: " << psatResult.getOptimizationRating() << endl;
+    MotorCurrent mc(200,1780,Motor::LineFrequency::FREQ60,Motor::EfficiencyClass::ENERGY_EFFICIENT,0,.25,460,225.8);
+    cout << mc.calculate() << endl;
+    MotorCurrent mc1(200,1780,Motor::LineFrequency::FREQ60,Motor::EfficiencyClass::ENERGY_EFFICIENT,0,1.25,460,225.8);
+    cout << mc1.calculate() << endl;
+    MotorCurrent mc2(200,1780,Motor::LineFrequency::FREQ60,Motor::EfficiencyClass::ENERGY_EFFICIENT,0,1.5001,460,225.8);
+    cout << mc2.calculate() << endl;
 
 }
+
