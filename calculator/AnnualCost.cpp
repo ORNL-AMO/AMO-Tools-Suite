29247535d9a1c6dea3bc02e32692edaa478f01e9
ehm <mishras@ornl.gov>
2016-11-07 08:11:04 -0500

    Comments for AnnualCost

--- calculator/AnnualCost.cpp
+++ calculator/AnnualCost.cpp
@@ -1,14 +1,22 @@
-//
-// Created by Accawi, Gina K. on 8/19/16.
-//
+/**
+ * @brief Calculates the annual energy cost
+ *
+ * @author Subhankar Mishra (mishras)
+ * @author Gina Accawi (accawigk)
+ * @bug No known bugs.
+ *
+ */
+
 #include "AnnualCost.h"
 
+/**
+ * Calculate Annual energy cost.
+ * Annual energy cost = MWh/year * 1000 * energy cost rate ($/kWh)
+ * 1000 in above equation is not used in excel version of the tool.
+ *
+ * @return Annual Energy Cost
+ */
 double AnnualCost::calculate() {
-    /*
-     * Calculate Annual energy cost
-     * Annual energy cost = MWh/year * 1000 * energy cost rate ($/kWh)
-     * 1000 in above equation is not used in excel.
-     */
     annualEnergyCost_ = annualEnergy_ * kwhRate_;
     return annualEnergyCost_;
 }
