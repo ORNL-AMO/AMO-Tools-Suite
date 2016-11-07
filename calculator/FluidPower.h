4f944ea1786ffef0f7a5838957d5c7ce5688bfa8
ehm <mishras@ornl.gov>
2016-11-07 13:08:32 -0500

    Comments for FluidPower

--- calculator/FluidPower.h
+++ calculator/FluidPower.h
@@ -1,3 +1,16 @@
+/**
+ * @brief Contains the definition of functions of CurveFitVal class.
+ *      calculate(): Calculates the curve fit value
+ *
+ * Given a set of x and y coordinates and degree of polynomial curve to fit, it calculates the curve fit value
+ * for a particular x coordinate value (read load factor here).
+ *
+ * @author Subhankar Mishra (mishras)
+ * @author Gina Accawi (accawigk)
+ * @bug No known bugs.
+ *
+ */
+
 //
 // Created by Mishra, Subhankar on 10/13/16.
 //
