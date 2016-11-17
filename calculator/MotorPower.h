24460ac1c11087295bb1f4b08934046310c9c951
ehm <mishras@ornl.gov>
2016-11-17 11:21:37 -0500

    Comments for MotorPower

--- calculator/MotorPower.h
+++ calculator/MotorPower.h
@@ -1,3 +1,13 @@
+/**
+ * @brief Contains the declaration of MotorPower class including the getters and setters as well as the calculators.
+ *          for motor efficiency.
+ *      calculate(): Calculates the motor power
+ *
+ * @author Subhankar Mishra (mishras)
+ * @bug No known bugs.
+ *
+ */
+
 //
 // Created by Accawi, Gina K. on 6/17/16.
 //
@@ -9,14 +19,34 @@
 
 class MotorPower {
 public:
+    /**
+     * Constrructor
+     * @param ratedVoltage Rated voltage of motor
+     * @param motorCurrent Motor current
+     * @param powerFactor Power factor of motor
+     * @return nothing
+     */
     MotorPower(double ratedVoltage, double motorCurrent, double powerFactor) :
             ratedVoltage_(ratedVoltage), motorCurrent_(motorCurrent), powerFactor_(powerFactor) {};
 
+    /**
+     * Calculates the motor power
+     * @return motor power
+     */
     double calculate();
 
 private:
+    /**
+     * Rated voltage of motor
+     */
     double ratedVoltage_;
+    /**
+     * Motor current
+     */
     double motorCurrent_;
+    /**
+     * Power factor of motor
+     */
     double powerFactor_;
 };
 
