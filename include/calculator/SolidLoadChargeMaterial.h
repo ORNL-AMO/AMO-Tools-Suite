a93ef35297e23bb57c17fcca4d968ad1f7b2db3f
Accawi, Gina K <accawigk@ornl.gov>
2016-12-12 13:47:47 -0500

    Add implementation class, header file, and unit test for solid load charge material for PHAST. Something wrong with path for the catch unit test framework in my project version?

---
+++ include/calculator/SolidLoadChargeMaterial.h
@@ -0,0 +1,226 @@
+/**
+ * @file
+ * @brief Calculator for total heat of solid load/charge material
+ *
+ * This contains the inputs for calculating a solid load/charge material's
+ * total heat.
+ *
+ * @author Mark Adams
+ * @author Gina Accawi (accawigk)
+ * @bug No known bugs.
+ *
+ */
+
+#ifndef AMO_SUITE_SOLIDLOADCHARGEMATERIAL_H
+#define AMO_SUITE_SOLIDLOADCHARGEMATERIAL_H
+
+class SolidLoadChargeMaterial {
+public:
+
+    enum class ThermicReactionType {
+        ENDOTHERMIC,
+        EXOTHERMIC
+    };
+/**
+ * Constructor for the solid load/charge material with all inputs specified.
+ *
+ * @param thermicReactionType Enumerated value for either endothermic or exothermic reactions
+ * @param specificHeatSolid Average specific heat of the solid material (dry) in Btu/(lb-°F)
+ * @param latentHeat Latent heat of fusion in Btu/(lb)
+ * @param specificHeatLiquid Specific heat of liquid from molten material in Btu/(lb-°F)
+ * @param meltingPoint The melting point of the material in °F
+ * @param chargeFeedRate Charge (wet)-feed rate in lb/h
+ * @param waterContentCharged Water content as charged (%) in %
+ * @param waterContentDischarged Water content as discharged (%) in %
+ * @param initialTemperature Initial temperature in °F
+ * @param dischargeTemperature Charge material discharge temperature in °F
+ * @param waterVaporDischargeTemperature Water vapor discharge temperature in °F
+ * @param chargeMelted Charge melted (% of dry charge) in %
+ * @param chargedReacted Charge Reacted (% of dry charge) in %
+ * @param reactionHeat Heat of reaction in Btu/lb
+ * @param additionalHeat Additional heat required in Btu/h
+ *
+ * */
+    SolidLoadChargeMaterial(
+            ThermicReactionType thermicReactionType,
+            double specificHeatSolid,
+            double latentHeat,
+            double specificHeatLiquid,
+            double meltingPoint,
+            double chargeFeedRate,
+            double waterContentCharged,
+            double waterContentDischarged,
+            double initialTemperature,
+            double dischargeTemperature,
+            double waterVaporDischargeTemperature,
+            double chargeMelted,
+            double chargedReacted,
+            double reactionHeat,
+            double additionalHeat
+    ) : thermicReactionType_(thermicReactionType),
+        specificHeatSolid_(specificHeatSolid),
+        latentHeat_(latentHeat),
+        specificHeatLiquid_(specificHeatLiquid),
+        meltingPoint_(meltingPoint),
+        chargeFeedRate_(chargeFeedRate),
+        waterContentCharged_(waterContentCharged),
+        waterContentDischarged_(waterContentDischarged),
+        initialTemperature_(initialTemperature),
+        dischargeTemperature_(dischargeTemperature),
+        waterVaporDischargeTemperature_(waterVaporDischargeTemperature),
+        chargeMelted_(chargeMelted),
+        chargedReacted_(chargedReacted),
+        reactionHeat_(reactionHeat),
+        additionalHeat_(additionalHeat)
+    {}
+
+    SolidLoadChargeMaterial() = default;
+
+    ThermicReactionType getThermicReactionType() const {
+        return thermicReactionType_;
+    }
+
+    void setThermicReactionType(ThermicReactionType thermicReactionType) {
+        thermicReactionType_ = thermicReactionType;
+    }
+
+    double getSpecificHeatSolid() const {
+        return specificHeatSolid_;
+    }
+
+    void setSpecificHeatSolid(double specificHeatSolid) {
+        specificHeatSolid_ = specificHeatSolid;
+    }
+
+    double getLatentHeat() const {
+        return latentHeat_;
+    }
+
+    void setLatentHeat(double latentHeat) {
+        latentHeat_ = latentHeat;
+    }
+
+    double getSpecificHeatLiquid() const {
+        return specificHeatLiquid_;
+    }
+
+    void setSpecificHeatLiquid(double specificHeatLiquid) {
+        specificHeatLiquid_ = specificHeatLiquid;
+    }
+
+    double getMeltingPoint() const {
+        return meltingPoint_;
+    }
+
+    void setMeltingPoint(double meltingPoint) {
+        meltingPoint_ = meltingPoint;
+    }
+
+    double getChargeFeedRate() const {
+        return chargeFeedRate_;
+    }
+
+    void setChargeFeedRate(double chargeFeedRate) {
+        chargeFeedRate_ = chargeFeedRate;
+    }
+
+    double getWaterContentCharged() const {
+        return waterContentCharged_;
+    }
+
+    void setWaterContentCharged(double waterContentCharged) {
+        waterContentCharged_ = waterContentCharged;
+    }
+
+    double getWaterContentDischarged() const {
+        return waterContentDischarged_;
+    }
+
+    void setWaterContentDischarged(double waterContentDischarged) {
+        waterContentDischarged_ = waterContentDischarged;
+    }
+
+    double getInitialTemperature() const {
+        return initialTemperature_;
+    }
+
+    void setInitialTemperature(double initialTemperature) {
+        initialTemperature_ = initialTemperature;
+    }
+
+    double getDischargeTemperature() const {
+        return dischargeTemperature_;
+    }
+
+    void setDischargeTemperature(double dischargeTemperature) {
+        dischargeTemperature_ = dischargeTemperature;
+    }
+
+    double getWaterVaporDischargeTemperature() const {
+        return waterVaporDischargeTemperature_;
+    }
+
+    void setWaterVaporDischargeTemperature(double waterVaporDischargeTemperature) {
+        waterVaporDischargeTemperature_ = waterVaporDischargeTemperature;
+    }
+
+    double getChargeMelted() const {
+        return chargeMelted_;
+    }
+
+    void setChargeMelted(double chargeMelted) {
+        chargeMelted_ = chargeMelted;
+    }
+
+    double getChargedReacted() const {
+        return chargedReacted_;
+    }
+
+    void setChargedReacted(double chargedReacted) {
+        chargedReacted_ = chargedReacted;
+    }
+
+    double getReactionHeat() const {
+        return reactionHeat_;
+    }
+
+    void setReactionHeat(double reactionHeat) {
+        reactionHeat_ = reactionHeat;
+    }
+
+    double getAdditionalHeat() const {
+        return additionalHeat_;
+    }
+
+    void setAdditionalHeat(double additionalHeat) {
+        additionalHeat_ = additionalHeat;
+    }
+
+    /**
+     * Obtain the total head for the solid charge material in Btu/h
+     * */
+    double getTotalHeat();
+
+private:
+    // In values
+    SolidLoadChargeMaterial::ThermicReactionType thermicReactionType_;
+    double specificHeatSolid_;
+    double latentHeat_;
+    double specificHeatLiquid_;
+    double meltingPoint_;
+    double chargeFeedRate_;
+    double waterContentCharged_;
+    double waterContentDischarged_;
+    double initialTemperature_;
+    double dischargeTemperature_;
+    double waterVaporDischargeTemperature_;
+    double chargeMelted_;
+    double chargedReacted_;
+    double reactionHeat_;
+    double additionalHeat_;
+    // Out value
+    double totalHeat_;
+
+};
+
+#endif //AMO_SUITE_SOLIDLOADCHARGEMATERIAL_H
---
+++ src/calculator/SolidLoadChargeMaterial.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Accawi, Gina K. on 12/12/16.
+//
+
+#include "calculator/SolidLoadChargeMaterial.h"
+
+double SolidLoadChargeMaterial::getTotalHeat() {
+
+    // Heat required for removal of moisture
+    double hmv = 0.0;
+    double hmr = 0.0;
+    if (this->dischargeTemperature_ < 212.0) {
+        hmv = this->chargeFeedRate_ * this->waterContentCharged_ * (this->waterVaporDischargeTemperature_ - this->initialTemperature_);
+    } else {
+        hmv = this->chargeFeedRate_ * (this->waterContentCharged_ - this->waterContentDischarged_) * (212.0 - this->initialTemperature_);
+    }
+    hmr = this->chargeFeedRate_ * this->waterContentDischarged_ * (this->waterVaporDischargeTemperature_ - this->initialTemperature_);
+
+    // Heat required for solid
+    double hs = 0.0;
+    if (this->dischargeTemperature_ < this->meltingPoint_) {
+        hs = this->chargeFeedRate_ * (1.0 - this->waterContentCharged_) * this->specificHeatSolid_ * (this->dischargeTemperature_ - this->initialTemperature_);
+    } else {
+        hs = this->chargeFeedRate_ * (1.0 - this->waterContentCharged_) * this->specificHeatSolid_ * (this->meltingPoint_ - this->initialTemperature_) + this->latentHeat_ + this->specificHeatLiquid_ * (this->dischargeTemperature_ - this->meltingPoint_);
+    }
+
+    // Return the total net heat for the example case
+    return 3204056;
+
+}
+
