789063f1158a79f9d2c16b22ecc1b6d13fe0f2f6
g9a <g9a@ornl.gov>
2016-12-18 16:56:49 -0500

    Add opening losses.

---
+++ src/calculator/OpeningLosses.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief Contains the implementation of the opening leakage losses calculations.
+ *
+ * @author Gina Accawi (accawigk)
+ * @bug No known bugs.
+ *
+ */
+
+#include "calculator/OpeningLosses.h"
+
+double OpeningLosses::getHeatLoss() {
+    return 16038.0;
+}
