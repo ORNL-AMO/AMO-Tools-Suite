fbe21cfeeccba183ad91025073356eb8f0716b77
g9a <g9a@ornl.gov>
2016-12-18 15:26:35 -0500

    Try repairing test case. Unit tests always are failing:  (Unknown expression after the reported line) due to a fatal error condition SIGABRT - Abort (abnormal termination) signal

---
+++ tests/GasCoolingLosses.unit.cpp
@@ -0,0 +1,8 @@
+#include "catch.hpp"
+#include <calculator/GasCoolingLosses.h>
+
+TEST_CASE( "Calculate Heat Loss for gas cooling Losses Air", "[Heat Loss]" ) {
+
+    REQUIRE( GasCoolingLosses(2500.0, 80.0, 280.0, 0.02).getHeatLoss() == Approx( 600000.0 ) );
+
+}
