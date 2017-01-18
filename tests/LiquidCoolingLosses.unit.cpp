19a701687a258a50cd6968ea06670e05acd5e373
Accawi, Gina K <accawigk@ornl.gov>
2017-01-18 10:45:09 -0500

    Calculate liquid cooling losses. Include correction factor

--- tests/LiquidCoolingLosses.unit.cpp
+++ tests/LiquidCoolingLosses.unit.cpp
@@ -2,5 +2,5 @@
 #include <calculator/LiquidCoolingLosses.h>
 
 TEST_CASE( "Calculate Heat Loss for liquid cooling Losses ethylene glycon", "[Heat Loss][Ethylene Glycon][Liquid]") {
-    CHECK(LiquidCoolingLosses(100.0, 9.35, 80.0, 210.0, 0.52).getHeatLoss() == Approx( 3792360.0));
+    CHECK(LiquidCoolingLosses(100.0, 9.35, 80.0, 210.0, 0.52, 1.0).getHeatLoss() == Approx( 3792360.0));
 }
