a2310ded53109e7642e0aebc421078999fe1c2c6
g9a <accawigk@ornl.gov>
2017-01-18 18:26:07 -0500

    Start flue gas calculations

---
+++ src/calculator/FlueGas.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * @brief Contains the implementation of the flue gas loss calculations.
+ *
+ * @author Gina Accawi (accawigk)
+ * @bug No known bugs.
+ *
+ */
+
+#include <cmath>
+#include "calculator/FlueGas.h"
+
+double FlueGas::getHeatLoss() {
+    double t_furnace = this->specificGravity_ * ( 520.0 / (460.0 + this->leakageTemperature_));
+    double cfh = 1655.0 * 0.8052 * (this->openingArea_ * 144.0) * pow((this->furnaceDraft_ / t_furnace), 0.5);
+    double scfh = cfh * ( 520.0 / ( 460.0 + this->leakageTemperature_));
+    double specific_heat_gasses_avg_temperature = (this->leakageTemperature_ + 60.0) / 2.0;
+
+}
