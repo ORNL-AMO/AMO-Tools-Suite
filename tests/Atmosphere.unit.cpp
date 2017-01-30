9e52de9fc1fb37478d1fae6e86c8d2a27e6005ba
Kyle <kdbeanblossom1@gmail.com>
2017-01-30 15:23:54 -0500

    Fixed header file for MotorShaftPower.h

--- tests/Atmosphere.unit.cpp
+++ tests/Atmosphere.unit.cpp
@@ -2,5 +2,5 @@
 #include <calculator/Atmosphere.h>
 
 TEST_CASE( "Calculate Total Heat for Atmospheric Gas", "[Total Heat][Atmosphere][Gas]") {
-    CHECK( Atmosphere(100.0, 1400.0, 1200.0, 1.0, 0.02).getTotalHeat() == Approx(0.0));
+    CHECK( Atmosphere(100.0, 1400.0, 1200.0, 1.0, 0.02).getTotalHeat() == Approx(31200.0));
 }
