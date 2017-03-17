6f8853e0b50dadd5a061232bbc31c1eed11f1086
Preston Shires <pshires1@vols.utk.edu>
2017-03-17 18:03:00 -0400

    add untested implementation of gas leakage losses calculations

--- src/calculator/losses/LeakageLosses.cpp
+++ src/calculator/losses/LeakageLosses.cpp
@@ -2,13 +2,20 @@
  * @file
  * @brief Contains the implementation of the gas leakage losses calculations.
  *
- * @author Gina Accawi (accawigk)
+ * @author Gina Accawi (accawigk), Preston Shires (pshires)
  * @bug No known bugs.
  *
  */
 
+#include <cmath>
 #include "calculator/losses/LeakageLosses.h"
 
 double LeakageLosses::getExfiltratedGasesHeatContent() {
-    return 2850767.0;
+    const double tFurnace = specificGravity_ * ( 520.0 / ( 460 + leakageGasTemperature_ ) );
+    const double specificHeatGassesAvgTemp = (leakageGasTemperature_ + 60.0) / 2.0;
+    const double cfh = 1655 * 0.8052 * ( openingArea_ * 10.764 * 144 ) * pow( draftPressure_ / tFurnace, 0.5 );
+    const double scfh = cfh * pow( 520.0 / ( 460.0 + leakageGasTemperature_ ), 0.5 );
+    exfiltratedGasesHeatContent_ = cfh * specificHeatGassesAvgTemp * (leakageGasTemperature_ - ambientTemperature_);
+
+    return exfiltratedGasesHeatContent_;
 }
