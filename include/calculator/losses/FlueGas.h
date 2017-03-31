68379d3132bd41f11963cc4c687339710865c0ea
Preston Shires <pshires1@vols.utk.edu>
2017-03-31 19:16:49 -0400

    Flesh out and modify the setup for the Flue Gas calculations

--- include/calculator/losses/FlueGas.h
+++ include/calculator/losses/FlueGas.h
@@ -12,22 +12,69 @@
 #ifndef AMO_SUITE_FLUEGAS_H
 #define AMO_SUITE_FLUEGAS_H
 
+#include <unordered_map>
+#include <functional>
+#include <string>
+
+class gasProperties {
+public:
+	gasProperties( std::function< double ( double t ) > specificHeat, const double molecularWeight, const double specificWeight) :
+			specificHeat(specificHeat), molecularWeight(molecularWeight), specificWeight(specificWeight)
+	{};
+
+	std::function< double ( double t ) > specificHeat;
+	// TODO rename userInput to something better (it's just user definied gas percentage), use xBar, x double bar etc
+	double userInput, xBar, xBarBar, molecularWeight, specificWeight;
+};
+
 class gasComposition {
 public:
 	gasComposition(const double CH4, const double C2H6, const double N2, const double H2,
 	               const double C3H8, const double C4H10_CnH2n, const double H2O, const double CO,
 	               const double CO2, const double SO2, const double O2) :
-			CH4(CH4), C2H6(C2H6), N2(N2), H2(H2), C3H8(C3H8), C4H10_CnH2n(C4H10_CnH2n), H2O(H2O),
-			CO(CO), CO2(CO2), SO2(SO2), O2(O2)
+			CH4(gasProperties([] (double t) { return 4.23 + 0.01177 * t; }, 16.042, 0.042417) ),
+			C2H6(gasProperties([] (double t) { return 4.04 + 0.01636 * t; }, 30.068, 0.079503) ),
+			N2(gasProperties([] (double t){ return 9.47 - 3.47 * 1000 / t + 1.07 * pow(10, 6) / (t * t); }, 28.016, 0.074077) ),
+			// TODO finish lambda functions, and check order of operations on above lambda expression
+			H2(gasProperties([] (double t){ return 4.23 + 0.01177 * t; }, 10, 10) ),
+			C3H8(gasProperties([] (double t) { return 4.23 + 0.01177 * t; }, 10, 10) ),
+			C4H10_CnH2n(gasProperties([] (double t) { return 4.23 + 0.01177 * t; }, 10, 10) ),
+			H2O(gasProperties([] (double t) { return 4.23 + 0.01177 * t; }, 10, 10) ),
+			CO(gasProperties([] (double t) { return 4.23 + 0.01177 * t; }, 10, 10) ),
+			CO2(gasProperties([] (double t) { return 4.23 + 0.01177 * t; }, 10, 10) ),
+			SO2(gasProperties([] (double t) { return 4.23 + 0.01177 * t; }, 10, 10) ),
+			O2(gasProperties([] (double t) { return 4.23 + 0.01177 * t; }, 10, 10) )
+
+
+//			CH4(CH4), C2H6(C2H6), N2(N2), H2(H2), C3H8(C3H8), C4H10_CnH2n(C4H10_CnH2n), H2O(H2O),
+//			CO(CO), CO2(CO2), SO2(SO2), O2(O2),
+//			totalPercent(CH4 + C2H6 + N2 + H2 + C3H8 + C4H10_CnH2n + H2O + CO + CO2 + SO2 + O2)
 	{}
 
 	gasComposition( const gasComposition & composition ) :
 			CH4(composition.CH4), C2H6(composition.C2H6), N2(composition.N2), H2(composition.H2),
 			C3H8(composition.C3H8), C4H10_CnH2n(composition.C4H10_CnH2n), H2O(composition.H2O),
 			CO(composition.CO), CO2(composition.CO2), SO2(composition.SO2), O2(composition.O2)
+//	        totalPercent(CH4 + C2H6 + N2 + H2 + C3H8 + C4H10_CnH2n + H2O + CO + CO2 + SO2 + O2)
 	{}
 
-	const double CH4, C2H6, N2, H2, C3H8, C4H10_CnH2n, H2O, CO, CO2, SO2, O2;
+	const gasProperties CH4, C2H6, N2, H2, C3H8, C4H10_CnH2n, H2O, CO, CO2, SO2, O2;
+//	const double totalPercent;
+
+// this are the correct specific weights for each molecule according to the table
+//	const std::unordered_map < std::string, const double > specificWeight = {
+//			{"CH4", 0.042417},
+//			{"C2H6", 0.079503},
+//			{"N2", 0.074077},
+//			{"H2", 0.005331},
+//			{"C3H8", 0.116589},
+//			{"C4H10_CnH2n", 0.153675},
+//			{"H2O", 0.047636},
+//			{"CO", 0.074061},
+//			{"CO2", 0.116367},
+//			{"SO2", 0.169381},
+//			{"O2", 0.084611}
+//	};
 };
 
 class FlueGas {
--- tests/FlueGas.unit.cpp
+++ tests/FlueGas.unit.cpp
@@ -3,9 +3,10 @@
 
 TEST_CASE( "Calculate Heat Loss for flue gas Losses", "[Heat Loss]" ) {
 	const gasComposition composition(94.1, 2.4, 1.41, 0.03, 0.49, 0.29, 0, 0.42, 0.71, 0, 0);
+//	auto testVal1 = composition.CH4.specificHeat( 200 );
+//	auto testVal2 = composition.C2H6.specificHeat( 200 );
 	const double whereIsTheOxygenPercentageInput = -1;
 
-	REQUIRE(FlueGas(700, whereIsTheOxygenPercentageInput, 9.0, 125, composition).getHeatLoss() == Approx(1924294214.0));
-//    REQUIRE(FlueGas(0.1, 3.0, 1600.0).getHeatLoss() == Approx(2850767.0 ) );
-
+	auto const randomAnswer = -1;
+	REQUIRE(FlueGas(700, whereIsTheOxygenPercentageInput, 9.0, 125, composition).getHeatLoss() == Approx(randomAnswer));
 }
