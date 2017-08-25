daa36606801323da854b7b2f4423a0da529fc9ce
Preston Shires <pshires1@vols.utk.edu>
2017-08-25 09:34:42 -0400

    start adding rounding to js unit tests, so that windows, mac, and linux can all pass without any fussing

--- tests/js/energyInput.js
+++ tests/js/energyInput.js
@@ -5,6 +5,10 @@ const test = require('tap').test
     , testRoot = require('path').resolve(__dirname, '../../')
     , bindings = require('bindings')({ module_root: testRoot, bindings: 'phast'});
 
+function rnd(value) {
+    return Number(Math.round(value + 'e' + 6) + 'e-' + 6);
+}
+
 test('phast', function (t) {
     t.plan(4);
     t.type(bindings.energyInput, 'function');
@@ -19,7 +23,7 @@ test('phast', function (t) {
     inp.otherFuels = 20;
     inp.electricityInput = 18000;
     var res = bindings.energyInput(inp);
-    t.equal(res.kwhCycle, 30978.898007031734, 'res.kwhCycle is ' + res.kwhCycle);
+    t.equal(rnd(res.kwhCycle), rnd(30978.898007031734), 'res.kwhCycle is ' + res.kwhCycle);
     t.equal(res.heatDelivered, 55.7, 'res.heatDelivered is ' + res.heatDelivered);
-    t.equal(res.totalKwhCycle, 48978.898007031734, 'res.totalKwhCycle is ' + res.totalKwhCycle);
+    t.equal(rnd(res.totalKwhCycle), rnd(48978.898007031734), 'res.totalKwhCycle is ' + res.totalKwhCycle);
 });
