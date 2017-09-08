6aab5d840d52025e3b012d7c53808418877bc057
Unknown <armstrongko@ornl.gov>
2017-09-08 12:10:12 -0400

    Fix unit conversion and unit tests

--- src/calculator/losses/EnergyInputEAF.cpp
+++ src/calculator/losses/EnergyInputEAF.cpp
@@ -11,10 +11,11 @@
 double EnergyInputEAF::getTotalChemicalEnergyInput() {
     const double coalHeatContent = (coalHeatingValue_ * coalCarbonInjection_) / 1000000;
     const double electrodeHeatContent = (electrodeUse_ * electrodeHeatingValue_) / 1000000;
-    return naturalGasHeatInput_ + coalHeatContent + electrodeHeatContent + otherFuels_;
+    return (naturalGasHeatInput_ + coalHeatContent + electrodeHeatContent + otherFuels_) * 1000000;
 }
 double EnergyInputEAF::getHeatDelivered() {
     const double coalHeatContent = (coalHeatingValue_ * coalCarbonInjection_) / 1000000;
     const double electrodeHeatContent = electrodeUse_ * electrodeHeatingValue_ / 1000000;
-    return naturalGasHeatInput_ + coalHeatContent + electrodeHeatContent + otherFuels_ + electricityInput_;
+    const double electricityHeat = electricityInput_ * 3412
+    return (naturalGasHeatInput_ + coalHeatContent + electrodeHeatContent + otherFuels_ + electricityHeat) * 1000000;
 }
--- tests/ExhaustGasEAF.unit.cpp
+++ tests/ExhaustGasEAF.unit.cpp
@@ -4,6 +4,6 @@
 TEST_CASE( "Calculate the Total Heat Exhaust", "[Total Heat Exhaust][ExhaustGasEAF][EAF]") {
 //    CHECK( ExhaustGasEAF(2800, 10, 10, 0, 5, 5, 50000, .005).getTotalHeatExhaust() == Approx(171958895.705521));
 
-    CHECK( ExhaustGasEAF(2800, 12, 10, 0, 8, 3, 8000, 0.001).getTotalHeatExhaust() == Approx(7331.3));
-    CHECK( ExhaustGasEAF(2800, 8, 6, 0, 8, 3, 6500, 0.001).getTotalHeatExhaust() == Approx(5014.4));
+    CHECK( ExhaustGasEAF(2800, 12, 10, 0, 8, 3, 8000, 0.001).getTotalHeatExhaust() == Approx(12553119.02));
+    CHECK( ExhaustGasEAF(2800, 8, 6, 0, 8, 3, 6500, 0.001).getTotalHeatExhaust() == Approx(8591939.26));
 }
