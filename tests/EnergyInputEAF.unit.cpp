da7c54f56ca5407bd9ab75625398a0fc19e8d200
pshires <pshires1@vols.utk.edu>
2017-09-08 12:33:31 -0400

    add semicolon and annotate the broken unit tests with numbers

--- tests/EnergyInputEAF.unit.cpp
+++ tests/EnergyInputEAF.unit.cpp
@@ -2,13 +2,13 @@
 #include <calculator/losses/EnergyInputEAF.h>
 
 TEST_CASE( "Calculate the Total Chemical Energy Input", "[Total Chemical Energy Input][EnergyInputEAF][Furnace]") {
-    CHECK(EnergyInputEAF(15, 56000, 900, 9000, 200, 12000, 0, 18000).getTotalChemicalEnergyInput() == Approx(47940000));
+    CHECK(EnergyInputEAF(15, 56000, 900, 9000, 200, 12000, 0, 18000).getTotalChemicalEnergyInput() == Approx(47940000)); // broken, 25500000.0
 
     CHECK( EnergyInputEAF(50, 6500, 3300, 9000, 500, 12000, 20, 18000).getTotalChemicalEnergyInput() == Approx(105700000));
 }
 
 TEST_CASE( "Calculate the Heat Delivered", "[Heat Delivered][EnergyInputEAF][Furnace]") {
-    CHECK( EnergyInputEAF(50, 6500, 3300, 9000, 500, 12000, 20, 18000).getHeatDelivered() == Approx(334232000));
+    CHECK( EnergyInputEAF(50, 6500, 3300, 9000, 500, 12000, 20, 18000).getHeatDelivered() == Approx(334232000)); // broken, 61416105700000.0
 
-    CHECK(EnergyInputEAF(15, 56000, 900, 9000, 200, 12000, 0, 18000).getHeatDelivered() == Approx(218655831));
+    CHECK(EnergyInputEAF(15, 56000, 900, 9000, 200, 12000, 0, 18000).getHeatDelivered() == Approx(218655831)); // broken, 61416025500000.0
 }
