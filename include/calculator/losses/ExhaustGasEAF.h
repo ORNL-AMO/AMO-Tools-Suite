0c74b6baa57ac5909f7d5d03fd015790d7134eb4
Preston Shires <pshires1@vols.utk.edu>
2017-09-29 16:58:23 -0400

    add implementations of pure C++ constructor, set methods, and calculate method in cpp source file. This leads to a broken nodeJS symbol binding issue
    dyld: lazy symbol binding failed: Symbol not found: __ZN13ExhaustGasEAF13setOffGasTempEd
      Referenced from: /Users/p69/AMO/AMO-Tools-Suite/build/Release/exhaustGasEAF.node
      Expected in: flat namespace
    
    dyld: Symbol not found: __ZN13ExhaustGasEAF13setOffGasTempEd
      Referenced from: /Users/p69/AMO/AMO-Tools-Suite/build/Release/exhaustGasEAF.node
      Expected in: flat namespace

--- include/calculator/losses/ExhaustGasEAF.h
+++ include/calculator/losses/ExhaustGasEAF.h
@@ -28,15 +28,15 @@ public:
 	 * @return nothing
 	 *
 	 * */
-//	ExhaustGasEAF(double offGasTemp, double CO, double H2, double combustibleGases, double vfr, double dustLoading);
+	ExhaustGasEAF(double offGasTemp, double CO, double H2, double combustibleGases, double vfr, double dustLoading);
 
-	ExhaustGasEAF(const double offGasTemp, const double CO, const double H2,
-	                             const double combustibleGases, const double vfr, const double dustLoading)
-			: offGasTemp(offGasTemp), CO(CO), H2(H2), combustibleGases(combustibleGases),
-			  vfr(vfr), dustLoading(dustLoading)
-	{
-		calculateTotalHeatExhaust();
-	}
+//	ExhaustGasEAF(const double offGasTemp, const double CO, const double H2,
+//	                             const double combustibleGases, const double vfr, const double dustLoading)
+//			: offGasTemp(offGasTemp), CO(CO), H2(H2), combustibleGases(combustibleGases),
+//			  vfr(vfr), dustLoading(dustLoading)
+//	{
+//		calculateTotalHeatExhaust();
+//	}
 
 	double getTotalHeatExhaust() const { return totalHeatExhaust; }
 	double getOffGasTemp() const { return offGasTemp; }
@@ -47,61 +47,61 @@ public:
 	double getDustLoading() const { return dustLoading; }
 
 
-	void setOffGasTemp(double offGasTemp) {
-		this->offGasTemp = offGasTemp;
-		calculateTotalHeatExhaust();
-	}
+//	void setOffGasTemp(double offGasTemp) {
+//		this->offGasTemp = offGasTemp;
+//		calculateTotalHeatExhaust();
+//	}
+//
+//	void setCO(double CO) {
+//		this->CO = CO;
+//		calculateTotalHeatExhaust();
+//	}
+//
+//	void setH2(double H2) {
+//		this->H2 = H2;
+//		calculateTotalHeatExhaust();
+//	}
+//
+//	void setCombustibleGases(double combustibleGases) {
+//		this->combustibleGases = combustibleGases;
+//		calculateTotalHeatExhaust();
+//	}
+//
+//	void setVfr(double vfr) {
+//		this->vfr = vfr;
+//		calculateTotalHeatExhaust();
+//	}
+//
+//	void setDustLoading(double dustLoading) {
+//		this->dustLoading = dustLoading;
+//		calculateTotalHeatExhaust();
+//	}
 
-	void setCO(double CO) {
-		this->CO = CO;
-		calculateTotalHeatExhaust();
-	}
-
-	void setH2(double H2) {
-		this->H2 = H2;
-		calculateTotalHeatExhaust();
-	}
-
-	void setCombustibleGases(double combustibleGases) {
-		this->combustibleGases = combustibleGases;
-		calculateTotalHeatExhaust();
-	}
-
-	void setVfr(double vfr) {
-		this->vfr = vfr;
-		calculateTotalHeatExhaust();
-	}
-
-	void setDustLoading(double dustLoading) {
-		this->dustLoading = dustLoading;
-		calculateTotalHeatExhaust();
-	}
-
-//	void setOffGasTemp(double offGasTemp);
-//	void setCO(double CO);
-//	void setH2(double H2);
-//	void setCombustibleGases(double combustibleGases);
-//	void setVfr(double vfr);
-//	void setDustLoading(double dustLoading);
+	void setOffGasTemp(double offGasTemp);
+	void setCO(double CO);
+	void setH2(double H2);
+	void setCombustibleGases(double combustibleGases);
+	void setVfr(double vfr);
+	void setDustLoading(double dustLoading);
 
 protected:
 	double offGasTemp, CO, H2, combustibleGases, vfr, dustLoading;
 	double totalHeatExhaust = 0;
 
-//	void calculateTotalHeatExhaust();
+	void calculateTotalHeatExhaust();
 
-	void calculateTotalHeatExhaust() {
-		const double COheatingValue = CO * 321 / 100;
-		const double H2heatingValue = H2 * 325 / 100;
-		const double combustibles = combustibleGases * 1020 / 100;
-		const double chemicalHeat = COheatingValue + H2heatingValue + combustibles;
-
-		const double sensibleHeat = 0.0225 * (offGasTemp - 60);
-		const double heatInDust = dustLoading * 0.25 * (offGasTemp - 60);
-		const double totalHeatFlue = chemicalHeat + sensibleHeat + heatInDust;
-		const double totalVfr = vfr * 520 / (460 + offGasTemp);
-		totalHeatExhaust = totalVfr * totalHeatFlue * 60;
-	}
+//	void calculateTotalHeatExhaust() {
+//		const double COheatingValue = CO * 321 / 100;
+//		const double H2heatingValue = H2 * 325 / 100;
+//		const double combustibles = combustibleGases * 1020 / 100;
+//		const double chemicalHeat = COheatingValue + H2heatingValue + combustibles;
+//
+//		const double sensibleHeat = 0.0225 * (offGasTemp - 60);
+//		const double heatInDust = dustLoading * 0.25 * (offGasTemp - 60);
+//		const double totalHeatFlue = chemicalHeat + sensibleHeat + heatInDust;
+//		const double totalVfr = vfr * 520 / (460 + offGasTemp);
+//		totalHeatExhaust = totalVfr * totalHeatFlue * 60;
+//	}
 };
 
 #endif //AMO_TOOLS_SUITE_EXHAUSTGASEAF_H
