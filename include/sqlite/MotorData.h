d4336c5eea9571d9ebb4bfa1dd9d89aebf825ec7
pshires <pshires1@vols.utk.edu>
2017-12-06 15:46:43 -0500

    add real test data to the motor database

--- include/sqlite/MotorData.h
+++ include/sqlite/MotorData.h
@@ -5,12 +5,21 @@ class MotorData;
 
 std::vector<MotorData> SQLite::get_default_motor_data() {
 	return {
-			{"KTM", "sickMotor", "Best Motors 2018", "beastMotorType", 900, 3600, 1800, "fullyEnclosed", "frame no. 25",
-			          360, "to be the best motor", 0, 1, 1, 1, 1.5, "fullyInsulated", 845, 136000, 28, 4.5, 1, 2, 85, 87, 89, 83,
-			          10, 11, 12, 13, 400, 300, 200, 225, 75, 99, 15, 30, 200, 175},
-			{"Honda", "some other motor", "Motors 2018", "someMotorType", 900, 3600, 1800, "partiallyEnclosed", "frame no. 15",
-						360, "to be a good motor", 0, 1, 1, 1, 1.5, "somewhat Insulated", 845, 136000, 28, 4.5, 1, 2, 85, 87, 89, 83,
-						10, 11, 12, 13, 400, 300, 200, 225, 75, 99, 15, 30, 200, 175}
+			{
+					"GE", "X$D Ultra IEEE 841", "M9455", "NEMA Design B", 50, 1800, 1780, "TEFC", "326T", 460,
+					"IEEE 841 Petroleum/Chemical", 0, 0, 0, 0, 1.15, "F", 511, 4, 615, 99.5, 5, 38, 48, 94.5, 94.7,
+					94.3, 91.6, 78, 73.6, 63.3, 41.5, 147.4, 294.8, 206.4, 63.5, 25.7, 362.5, 92.9, 115.2, 2000
+			},
+			{
+					"WEG Electric", "W22-NEMA Premium SD", "20018ET3G447", "NEMA Design B", 200, 1800, 0, "TEFC",
+					"447/9T", 460, "undefined", 0, 0, 0, 0, 1.15, "undefined", 1899, 21098, 0, 0, 0, 0, 96.2, 96.2,
+					95.4, 0, 85, 82, 73, 0, 582, 1455, 1396.8, 230, 0, 1564, 16, 35, 0, 0
+			},
+			{
+					"Teco/Westinghouse", "MAX-E2/841", "HB2004", "NEMA Design B", 200, 1800, 1786, "TEFC", "447T", 460,
+					"undefined", 0, 0, 0, 0, 1.15, "F", 2420, 23132, 0, 0, 0, 0, 96.2, 95.8, 95,0, 84.5, 82.5, 78.5,0,
+					588, 1234.8, 705.6, 230, 46.4, 1450, 0, 0, 0, 0
+			}
 	};
 }
 
--- tests/SQLite.unit.cpp
+++ tests/SQLite.unit.cpp
@@ -872,14 +872,23 @@ TEST_CASE( "SQLite - Motor Data", "[sqlite][motor]" ) {
     {
         auto const motors = sqlite.getMotorData();
 
-        auto const expected = MotorData("KTM", "sickMotor", "Best Motors 2018", "beastMotorType", 900, 3600, 1800, "fullyEnclosed", "frame no. 25",
-                  360, "to be the best motor", 0, 1, 1, 1, 1.5, "fullyInsulated", 845, 136000, 28, 4.5, 1, 2, 85, 87, 89, 83,
-                  10, 11, 12, 13, 400, 300, 200, 225, 75, 99, 15, 30, 200, 175);
+        auto const expected = MotorData(
+                "GE", "X$D Ultra IEEE 841", "M9455", "NEMA Design B", 50, 1800, 1780, "TEFC", "326T", 460,
+                "IEEE 841 Petroleum/Chemical", 0, 0, 0, 0, 1.15, "F", 511, 4, 615, 99.5, 5, 38, 48, 94.5, 94.7,
+                94.3, 91.6, 78, 73.6, 63.3, 41.5, 147.4, 294.8, 206.4, 63.5, 25.7, 362.5, 92.9, 115.2, 2000
+        );
 
 	    compare(motors[0], expected);
 
-	    auto const motor = sqlite.getMotorDataById(1);
-        compare(motor, expected);
+
+	    auto const expected2 = MotorData(
+			    "WEG Electric", "W22-NEMA Premium SD", "20018ET3G447", "NEMA Design B", 200, 1800, 0, "TEFC",
+			    "447/9T", 460, "undefined", 0, 0, 0, 0, 1.15, "undefined", 1899, 21098, 0, 0, 0, 0, 96.2, 96.2,
+			    95.4, 0, 85, 82, 73, 0, 582, 1455, 1396.8, 230, 0, 1564, 16, 35, 0, 0
+	    );
+
+	    auto const motor = sqlite.getMotorDataById(2);
+        compare(motor, expected2);
     }
 }
 
