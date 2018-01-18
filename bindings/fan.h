4de1fe4155ce78b5cc326278f7ed63e09d1890ed
pshires <pshires1@vols.utk.edu>
2018-01-18 17:50:04 -0500

    update getTraverseInputData function to have a real implementation

--- bindings/fan.h
+++ bindings/fan.h
@@ -52,56 +52,19 @@ void SetR(const char *nm, double n) {
 
 std::vector <std::vector<double>> getTraverseInputData(std::string const & innerObj) {
 	auto const plane = inp->ToObject()->Get(Nan::New<String>(innerObj).ToLocalChecked());
-	auto const array = plane->ToObject()->Get(Nan::New<String>("traverseData").ToLocalChecked());
+	auto const arrayTmp = plane->ToObject()->Get(Nan::New<String>("traverseData").ToLocalChecked());
+	auto const & array = v8::Local<v8::Array>::Cast(arrayTmp);
 
-	return {
-			{
-					0.701, 0.703, 0.6675, 0.815, 0.979, 1.09, 1.155, 1.320, 1.578, 2.130
-			},
-			{
-					0.690, 0.648, 0.555, 0.760, 0.988, 1.060, 1.100, 1.110, 1.458, 1.865
-			},
-			{
-					0.691, 0.621, 0.610, 0.774, 0.747, 0.835, 0.8825, 1.23, 1.210, 1.569
-			}
-	};
+	std::vector<std::vector <double> > traverseData(array->Length());
+	for (std::size_t i = 0; i < array->Length(); i++) {
+		auto const & innerArray = v8::Local<v8::Array>::Cast(array->Get(i)->ToObject());
+		traverseData.at(i).resize(innerArray->Length());
+		for (std::size_t j = 0; j < innerArray->Length(); j++) {
+			traverseData.at(i).at(j) = innerArray->Get(j)->NumberValue();
+		}
+	}
 
-//	Local<String> arrayStr = Nan::New<String>("inlets").ToLocalChecked();
-//	auto array = inp->ToObject()->Get(arrayStr);
-//	v8::Local<v8::Array> arr = v8::Local<v8::Array>::Cast(array);
-//
-//	auto pressureStr = Nan::New<String>("pressure").ToLocalChecked();
-//	auto temperatureStr = Nan::New<String>("temperature").ToLocalChecked();
-//	auto qualityStr = Nan::New<String>("quality").ToLocalChecked();
-//	auto massFlowStr = Nan::New<String>("massFlow").ToLocalChecked();
-//	auto specificEnthalpyStr = Nan::New<String>("specificEnthalpy").ToLocalChecked();
-//	auto specificEntropyStr = Nan::New<String>("specificEntropy").ToLocalChecked();
-//	auto energyFlowStr = Nan::New<String>("energyFlow").ToLocalChecked();
-//
-//	std::vector<Inlet> inlets;
-//
-//	for (std::size_t i = 0; i < arr->Length(); i++) {
-//		auto const pressure = arr->Get(i)->ToObject()->Get(pressureStr)->NumberValue();
-//		unsigned val = static_cast<unsigned>(arr->Get(i)->ToObject()->Get(Nan::New<String>("thermodynamicQuantity").ToLocalChecked())->NumberValue());
-//		auto const quantity = static_cast<SteamProperties::ThermodynamicQuantity>(val);
-//		auto const quantityValue = arr->Get(i)->ToObject()->Get(Nan::New<String>("quantityValue").ToLocalChecked())->NumberValue();
-//		auto const massFlow = arr->Get(i)->ToObject()->Get(massFlowStr)->NumberValue();
-//		inlets.emplace_back(Inlet(pressure, quantity, quantityValue, massFlow));
-//
-//		Local<Object> obj = Nan::New<Object>();
-//		Local<String> inletNum = Nan::New<String>("inlet" + std::to_string(i + 1)).ToLocalChecked();
-//		auto const inletProps = inlets[i].getInletProperties();
-//
-//		Nan::Set(obj, pressureStr, Nan::New<Number>(inletProps.at("pressure")));
-//		Nan::Set(obj, temperatureStr, Nan::New<Number>(inletProps.at("temperature")));
-//		Nan::Set(obj, qualityStr, Nan::New<Number>(inletProps.at("quality")));
-//		Nan::Set(obj, specificEnthalpyStr, Nan::New<Number>(inletProps.at("specificEnthalpy")));
-//		Nan::Set(obj, specificEntropyStr, Nan::New<Number>(inletProps.at("specificEntropy")));
-//		Nan::Set(obj, energyFlowStr, Nan::New<Number>(inlets[i].getInletEnergyFlow()));
-//		Nan::Set(obj, massFlowStr, Nan::New<Number>(inlets[i].getMassFlow()));
-//
-//		Nan::Set(r, inletNum, obj);
-//	}
+	return traverseData;
 }
 
 
