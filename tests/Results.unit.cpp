951937d3df8a9352a5df86278fa0de8fd81b00d2
Zach Fontenot <fontenotz@etsu.edu>
2018-08-09 15:45:48 -0400

    working tests for specified drive

--- tests/Results.unit.cpp
+++ tests/Results.unit.cpp
@@ -237,7 +237,7 @@ TEST_CASE( "PSATResults - existing changed voltage", "[PSAT results]" ) {
 	CHECK(ex.annualCost * 1000.0 == Approx(91405.352064809));
 }
 
-TEST_CASE( "PSATResults - existing changed voltage", "[PSAT results]" ) {
+TEST_CASE( "PSATResults - specified drive", "[PSAT results]" ) {
 	double achievableEfficiency = 90, pump_rated_speed = 1185, kinematic_viscosity = 1.0, specific_gravity = 0.99;
 	double stages = 1.0, motor_rated_power = 350, motor_rated_speed = 1185, efficiency = 95, motor_rated_voltage = 2300;
 	double motor_rated_fla = 83, margin = 0.15, operating_fraction = 1.0, cost_kw_hour = 0.039, flow_rate = 2800;
@@ -254,10 +254,10 @@ TEST_CASE( "PSATResults - existing changed voltage", "[PSAT results]" ) {
 	Pump::FieldData fd(flow_rate, head, loadEstimationMethod1, motor_field_power, motor_field_current, motor_field_voltage);
 	PSATResult psat(pump, motor, fd, baseline_pump_efficiency, operating_fraction, cost_kw_hour);
 	auto const & ex = psat.calculateExisting();
-	//CHECK(ex.pumpEfficiency * 100 == Approx(21.4684857877)); goes up
+	CHECK(ex.pumpEfficiency * 100 == Approx(22.5984060923));
 	CHECK(ex.motorRatedPower == Approx(350));
-	CHECK(ex.motorShaftPower == Approx(322.0343896));
-	CHECK(ex.pumpShaftPower == Approx(338.9835681041));
+	CHECK(ex.motorShaftPower == Approx(338.9835681041));
+	CHECK(ex.pumpShaftPower == Approx(322.0343896989));
 	CHECK(ex.motorEfficiency * 100 == Approx(94.518008321));
 	CHECK(ex.motorPowerFactor * 100 == Approx(83.4292940632));
 	CHECK(ex.motorCurrent == Approx(80.5));
