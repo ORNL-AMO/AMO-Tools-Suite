f51f5e8f58317c152c6da4879e91f5f291baf900
pshires <pshires10@gmail.com>
2018-10-16 18:28:18 -0400

    finish HeatExchanger, add unit tests

--- include/ssmt/HeatExchanger.h
+++ include/ssmt/HeatExchanger.h
@@ -5,60 +5,6 @@
 #include "SteamProperties.h"
 #include "Header.h"
 
-//class HeatExchanger {
-//public:
-//    struct Output {
-//        Output(SteamProperties hotOutlet, SteamProperties coldOutlet)
-//                : hotOutlet(hotOutlet), coldOutlet(coldOutlet)
-//        {}
-//        SteamProperties hotOutlet, coldOutlet;
-//    };
-//    /**
-//     * Constructor for the HeatExchanger calculator
-//     * @param hotInlet const Inlet, must be thermodynamic quantity of temperature or else exception will be thrown
-//     * @param coldInlet const Inlet, must be thermodynamic quantity of temperature or else exception will be thrown
-//     * @param approachTemp double, default of 20 K
-//     * */
-//    HeatExchanger(const Inlet hotInlet, const Inlet coldInlet, const double approachTemp = 20)
-//            : hotInlet(hotInlet), coldInlet(coldInlet), approachTemp(approachTemp) {
-//        if (hotInlet.getQuantityType() != SteamProperties::ThermodynamicQuantity::TEMPERATURE
-//            || coldInlet.getQuantityType() != SteamProperties::ThermodynamicQuantity::TEMPERATURE) {
-//            throw std::runtime_error("HeatExchanger.h - You must instantiate the Inlet input classes with the ThermodynamicQuantity TEMPERATURE");
-//        }
-//    }
-//
-//    Output calculate() {
-//        const double maxTempDiff = hotInlet.getQuantityValue() - coldInlet.getQuantityValue();
-//        auto hotOutletTest = SteamProperties(
-//                hotInlet.getPressure(), SteamProperties::ThermodynamicQuantity::TEMPERATURE, hotInlet.getQuantityValue()
-//        ).calculate();
-//
-//        double heatExchanged = hotInlet.getInletEnergyFlow() - hotOutletTest.internalEnergy;
-//        // TODO this should be rearranged to not perform a useless calculation
-//        auto coldOutletTest = SteamProperties(
-//                coldInlet.getPressure(), SteamProperties::ThermodynamicQuantity::ENTHALPY,
-//                (coldInlet.getInletEnergyFlow() + heatExchanged) / coldInlet.getMassFlow() * 1000
-//        ).calculate();
-//
-//        if ((hotOutletTest.temperature - coldInlet.getQuantityValue()) > approachTemp) {
-//            coldOutletTest = SteamProperties(
-//                    coldInlet.getPressure(), SteamProperties::ThermodynamicQuantity::TEMPERATURE,
-//                    hotInlet.getQuantityValue() - approachTemp
-//            ).calculate();
-//
-//            heatExchanged = coldOutletTest.internalEnergy - coldInlet.getInletEnergyFlow();
-//            hotOutletTest = SteamProperties(
-//                    hotInlet.getPressure(), SteamProperties::ThermodynamicQuantity::ENTHALPY,
-//                    (hotInlet.getInletEnergyFlow() - heatExchanged) / hotInlet.getMassFlow() * 1000;
-//            ).calculate();
-//        }
-//        return {hotOutletTest, coldOutletTest};
-//    }
-//private:
-//    Inlet hotInlet, coldInlet;
-//    const double approachTemp;
-//};
-
 class HeatExchanger {
 public:
     struct Output {
@@ -75,27 +21,31 @@ public:
      * */
     HeatExchanger(const SteamSystemModelerTool::FluidProperties hotInlet,
                   const SteamSystemModelerTool::FluidProperties coldInlet,
-                  const double approachTemp = 20)
+                  const double approachTemp = 20
+    )
             : hotInlet(hotInlet), coldInlet(coldInlet), approachTemp(approachTemp)
     {}
 
     Output calculate() {
-        const double maxTempDiff = hotInlet.temperature - coldInlet.temperature;
         auto sp = SteamProperties(
                 hotInlet.pressure, SteamProperties::ThermodynamicQuantity::TEMPERATURE,
                 coldInlet.temperature + approachTemp
         ).calculate();
 
-        auto hotOutletTest = SteamSystemModelerTool::FluidProperties(hotInlet.massFlow, sp.internalEnergy, sp);
+        auto hotOutletTest = SteamSystemModelerTool::FluidProperties(
+                hotInlet.massFlow, hotInlet.massFlow * sp.specificEnthalpy / 1000, sp
+        );
 
-        double heatExchanged = hotInlet.internalEnergy - hotOutletTest.internalEnergy;
+        double heatExchanged = hotInlet.energyFlow - hotOutletTest.energyFlow;
         // TODO this should be rearranged to not perform a useless calculation
         sp = SteamProperties(
                 coldInlet.pressure, SteamProperties::ThermodynamicQuantity::ENTHALPY,
-                (coldInlet.internalEnergy + heatExchanged) / coldInlet.massFlow * 1000
+                (coldInlet.energyFlow + heatExchanged) / coldInlet.massFlow * 1000
         ).calculate();
 
-        auto coldOutletTest = SteamSystemModelerTool::FluidProperties(coldInlet.massFlow, sp.internalEnergy, sp);
+        auto coldOutletTest = SteamSystemModelerTool::FluidProperties(
+                coldInlet.massFlow, coldInlet.massFlow * sp.specificEnthalpy / 1000, sp
+        );
 
         if ((hotOutletTest.temperature - coldInlet.temperature) > approachTemp) {
             sp = SteamProperties(
@@ -103,15 +53,19 @@ public:
                     hotInlet.temperature - approachTemp
             ).calculate();
 
-            coldOutletTest = SteamSystemModelerTool::FluidProperties(coldInlet.massFlow, sp.internalEnergy, sp);
+            coldOutletTest = SteamSystemModelerTool::FluidProperties(
+                    coldInlet.massFlow, coldInlet.massFlow * sp.specificEnthalpy / 1000, sp
+            );
 
-            heatExchanged = coldOutletTest.internalEnergy - coldInlet.internalEnergy;
+            heatExchanged = coldOutletTest.energyFlow - coldInlet.energyFlow;
             sp = SteamProperties(
                     hotInlet.pressure, SteamProperties::ThermodynamicQuantity::ENTHALPY,
-                    (hotInlet.internalEnergy - heatExchanged) / hotInlet.massFlow * 1000
+                    (hotInlet.energyFlow - heatExchanged) / hotInlet.massFlow * 1000
             ).calculate();
 
-            hotOutletTest = SteamSystemModelerTool::FluidProperties(hotInlet.massFlow, sp.internalEnergy, sp);
+            hotOutletTest = SteamSystemModelerTool::FluidProperties(
+                    hotInlet.massFlow, hotInlet.massFlow * sp.specificEnthalpy / 1000, sp
+            );
         }
         return {hotOutletTest, coldOutletTest};
     }
