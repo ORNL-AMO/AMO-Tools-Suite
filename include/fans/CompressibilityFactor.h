a17d83b380277526db9e84d4c34b1a73a2357a23
Allie Ledbetter <aeledbetter1999@gmail.com>
2019-03-13 13:42:23 -0400

    More updates to Fan Doxygen

--- include/fans/CompressibilityFactor.h
+++ include/fans/CompressibilityFactor.h
@@ -1,12 +1,14 @@
+
+/**
+ * @brief Contains some of the Fan related classes
+ *
+ * @author Preston Shires (pshires)
+ * @author Allie Ledbetter (Aeledbetter)
+ * @bug No known bugs.
+ *
+ */
 #ifndef AMO_TOOLS_SUITE_COMPRESSIBILITYFACTOR_H
 #define AMO_TOOLS_SUITE_COMPRESSIBILITYFACTOR_H
-/**
- * @file CompressibilityFactor.h
- * @author Preston Shires (pshires) & Allie Ledbetter (aeledbetter)
- * @brief Contains the calculator for compressability factor
- * @bug No known bugs.
- * 
- */
 
 #include <cmath>
 /**
@@ -32,7 +34,14 @@ public:
 			: moverShaftPower(moverShaftPower), inletPressure(inletPressure), outletPressure(outletPressure),
 			  barometricPressure(barometricPressure), flowRate(flowRate), specificHeatRatio(specificHeatRatio)
 	{}
-
+	/**
+	 * @param inletPressure double, in inches of water column, gauge
+	 * @param barometricPressure double, in Hg
+	 * @param outletPressure double, in inches of water column, gauge
+	 * @param moverShaftPower double, in horsepower
+	 * @param flowRate double, in cfm
+ 	 * @param specificHeatRatio double, unitless
+	 */
 	double calculate() {
 		double const p1 = inletPressure + 13.63 * barometricPressure;
 		double const x = (outletPressure - inletPressure) / p1;
@@ -42,6 +51,14 @@ public:
 	}
 
 private:
+/**
+ * @param moverShaftPower double, in horsepower
+ * @param inletPressure double, in inches of water column, gauge
+ * @param outletPressure double, in inches of water column, gauge
+ * @param barometricPressure double, in Hg
+ * @param flowRate double, in cfm
+ * @param specificHeatRatio double, unitless
+ */
 	const double moverShaftPower, inletPressure, outletPressure, barometricPressure, flowRate, specificHeatRatio;
 };
 
