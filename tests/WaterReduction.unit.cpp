8a89d36fbb60ac2071f16f2d7d8b4d1fd772eadf
shubhamkokul <shubhamkokul@gmail.com>
2019-07-10 14:54:12 -0400

    Water Reduction

--- tests/WaterReduction.unit.cpp
+++ tests/WaterReduction.unit.cpp
@@ -1,7 +1,7 @@
 #include <catch.hpp>
 #include "calculator/util/WaterReduction.h"
 
-TEST_CASE("Water Reduction MeteredFlowData") {
+TEST_CASE("Water Reduction MeteredFlowData", "[WaterReduction][util]") {
 std::vector<WaterReductionInput> waterReductionInputVec = {
         WaterReductionInput(8640, 0.005, 0,
                             MeteredFlowData(100),
@@ -12,12 +12,15 @@ std::vector<WaterReductionInput> waterReductionInputVec = {
 auto waterReduction = WaterReduction(waterReductionInputVec);
 auto testOutput = waterReduction.calculate();
 
-CHECK(testOutput.waterUse == Approx(51840));
-CHECK(testOutput.waterUse == Approx(51840));
-CHECK(testOutput.waterCost == Approx(259.2));
+CHECK(testOutput
+.waterUse == Approx(51840));
+CHECK(testOutput
+.waterUse == Approx(51840));
+CHECK(testOutput
+.waterCost == Approx(259.2));
 }
 
-TEST_CASE("Water Reduction VolumeMeterData") {
+TEST_CASE("Water Reduction VolumeMeterData", "[WaterReduction][util]") {
 std::vector<WaterReductionInput> waterReductionInputVec = {
         WaterReductionInput(8640, 0.005, 1,
                             MeteredFlowData(100),
@@ -31,12 +34,15 @@ auto testOutput = waterReduction.calculate();
 auto waterReduction = WaterReduction(waterReductionInputVec);
 auto testOutput = waterReduction.calculate();
 
-CHECK(testOutput.waterUse == Approx(58993));
-CHECK(testOutput.waterUse == Approx(58993));
-CHECK(testOutput.waterCost == Approx(294.965));
+CHECK(testOutput
+.waterUse == Approx(58993));
+CHECK(testOutput
+.waterUse == Approx(58993));
+CHECK(testOutput
+.waterCost == Approx(294.965));
 }
 
-TEST_CASE("Water Reduction BucketMethodData") {
+TEST_CASE("Water Reduction BucketMethodData", "[WaterReduction][util]") {
 std::vector<WaterReductionInput> waterReductionInputVec = {
         WaterReductionInput(8640, 0.005, 2,
                             MeteredFlowData(100),
@@ -50,12 +56,15 @@ auto testOutput = waterReduction.calculate();
 auto waterReduction = WaterReduction(waterReductionInputVec);
 auto testOutput = waterReduction.calculate();
 
-CHECK(testOutput.waterUse == Approx(15552));
-CHECK(testOutput.waterUse == Approx(15552));
-CHECK(testOutput.waterCost == Approx(77.76));
+CHECK(testOutput
+.waterUse == Approx(15552));
+CHECK(testOutput
+.waterUse == Approx(15552));
+CHECK(testOutput
+.waterCost == Approx(77.76));
 }
 
-TEST_CASE("Water Reduction WaterOtherMethodData") {
+TEST_CASE("Water Reduction WaterOtherMethodData", "[WaterReduction][util]") {
 std::vector<WaterReductionInput> waterReductionInputVec = {
         WaterReductionInput(8640, 0.005, 3,
                             MeteredFlowData(100),
@@ -69,7 +78,10 @@ auto testOutput = waterReduction.calculate();
 auto waterReduction = WaterReduction(waterReductionInputVec);
 auto testOutput = waterReduction.calculate();
 
-CHECK(testOutput.waterUse == Approx(15000));
-CHECK(testOutput.waterUse == Approx(15000));
-CHECK(testOutput.waterCost == Approx(75));
+CHECK(testOutput
+.waterUse == Approx(15000));
+CHECK(testOutput
+.waterUse == Approx(15000));
+CHECK(testOutput
+.waterCost == Approx(75));
 }
