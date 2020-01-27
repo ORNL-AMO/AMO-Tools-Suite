6f0ac0a1e1ee757626caab7539c1d1e5b59feb09
Colin Causey <causeyc@findlay.edu>
2020-01-27 13:54:58 -0500

    SQLite.h

--- include/sqlite/SQLite.h
+++ include/sqlite/SQLite.h
@@ -159,6 +159,13 @@ public:
     bool deleteMotorData(int id);
     bool updateMotorData(MotorData const & motor);
 
+    std::vector<PumpData> getPumpData() const;
+    std::vector<PumpData> getCustomPumpData() const;
+    PumpData getPumpDataById(int id) const;
+    bool insertPumpData(PumpData const & pump);
+    bool deletePumpData(int id);
+    bool updatePumpData(PumpData const & pump);
+
 private:
     // returns true if the material id falls in the default material id range
     inline bool isDefaultMaterial(const int id, std::size_t const defaultMaterialsSize) {
@@ -221,6 +228,13 @@ private:
     sqlite3_stmt * m_motor_data_update_stmt = nullptr;
     sqlite3_stmt * m_motor_data_delete_stmt = nullptr;
 
+    sqlite3_stmt * m_pump_data_insert_stmt = nullptr;
+    sqlite3_stmt * m_pump_data_select_stmt = nullptr;
+    sqlite3_stmt * m_pump_data_select_single_stmt = nullptr;
+    sqlite3_stmt * m_pump_data_select_custom_stmt = nullptr;
+    sqlite3_stmt * m_pump_data_update_stmt = nullptr;
+    sqlite3_stmt * m_pump_data_delete_stmt = nullptr;
+
     void create_select_stmt();
 
     void create_update_and_delete_stmt();
@@ -245,6 +259,8 @@ private:
 
     bool insert_motor_data(MotorData const & m);
 
+    bool insert_pump_data(PumpData const & pump);
+
     void insert_default_data();
 
     std::vector<SolidLoadChargeMaterial> get_default_solid_load_charge_materials();
@@ -262,6 +278,8 @@ private:
     std::vector<WallLosses> get_default_wall_losses_surface();
 
     std::vector<MotorData> get_default_motor_data();
+
+    std::vector<PumpData> get_default_pump_data();
 };
 
 
