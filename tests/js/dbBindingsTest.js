8c294e65d082dd087f88962b411c592619daeddf
Colin Causey <causeyc@findlay.edu>
2020-02-04 10:54:47 -0500

    Updated dbSelectPumpData test in dbBindingsTest.js

--- tests/js/dbBindingsTest.js
+++ tests/js/dbBindingsTest.js
@@ -509,8 +509,8 @@ test('dbSelectPumpData', function (t) {
     var test = bindings.selectPumps();
 
     //t.equal(res.id, obj.id, res.id + " != " + obj.id);
-    //t.equal(test.length, 0, test.length + " != 1");
-    t.equal(0, 0, "0 != 0");
+    t.equal(test.length, 1, test.length + " != 1");
+    //t.equal(0, 0, "0 != 0");
 
     // bindings.insertPump(pump_example);
     //var all_pumps = bindings.selectPumps();
