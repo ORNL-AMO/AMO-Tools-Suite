b6121a4d0a61dabcaf9b557806d0bb27b605f139
causeyc <47064193+causeyc@users.noreply.github.com>
2020-03-05 16:07:02 -0500

    Update package_release.sh

--- travis_scripts/package_release.sh
+++ travis_scripts/package_release.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-
 if (("$TRAVIS_BRANCH" == "$PRODUCTION"))
 then
   if (("$TRAVIS_OS_NAME" == "windows"))
   then
     cmake --build . --config Release --target PACKAGE
-    ls -a
-    ls -a Release/
   else
     make package
-    ls -a
   fi
 fi
