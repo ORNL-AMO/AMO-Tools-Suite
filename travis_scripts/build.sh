37b49b3b549fda80bfc07485a2c782ebdb557f5d
causeyc <47064193+causeyc@users.noreply.github.com>
2020-03-09 12:49:51 -0400

    Update build.sh

---
+++ travis_scripts/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ev
+
+cmake -D BUILD_TESTING:BOOL=ON -D BUILD_PACKAGE:BOOL=$PACKAGE ./
+cmake --build . --config Release
+
+if [[ $TRAVIS_OS_NAME == "windows" ]]; then
+  ./Release/amo_tools_suite_tests;
+else
+  ./bin/amo_tools_suite_tests;
+fi
+
+npm run test
+npm run at
+
+
+if [[ $TRAVIS_BRANCH == $PRODUCTION ]]; then
+  if [[ $TRAVIS_OS_NAME == 'windows' ]]; then
+    travis_retry cmake --build . --config Release --target PACKAGE;
+  else
+    make package;
+  fi
+else
+  echo "."
+fi
+
+
+exit 0;
