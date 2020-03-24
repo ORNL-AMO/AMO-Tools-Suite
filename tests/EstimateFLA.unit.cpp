94a86e856257c3a6b926d83ac067f1797ae32850
Colin Causey <causeyc@findlay.edu>
2020-03-24 14:22:44 -0400

    Upddated EstimateFLA unit test

--- tests/EstimateFLA.unit.cpp
+++ tests/EstimateFLA.unit.cpp
@@ -1,9 +1,38 @@
 #include "catch.hpp"
 #include <sqlite/SQLite.h>
+//#include <calculator/motor/MotorEfficiency.h>
 #include <calculator/motor/EstimateFLA.h>
+#include <fstream>
 
 
 TEST_CASE( "Estimate FLA", "[EstimateFLA]" ) {
- //CHECK(EstimateFLA(200, 1780, Motor::LineFrequency::FREQ60, Motor::EfficiencyClass::SPECIFIED, 94, 460).calculate() == Approx(227.3));
- //CHECK(EstimateFLA(200, 1780, Motor::LineFrequency::FREQ60, Motor::EfficiencyClass::SPECIFIED, 95, 460).calculate() == Approx(228.4));
+    auto fla1 = EstimateFLA(200, 1780, Motor::LineFrequency::FREQ60, Motor::EfficiencyClass::SPECIFIED, 94, 460);
+    auto fla2 = EstimateFLA(200, 1780, Motor::LineFrequency::FREQ60, Motor::EfficiencyClass::SPECIFIED, 95, 460);
+    std::ofstream fout;
+    fout.open("debug.txt", std::ios::app);
+    auto results1 = fla1.calculate();
+    auto results2 = fla2.calculate();
+    fout << "EstimateFLA.unit.cpp" << std::endl;
+    fout << "------------------------" << std::endl;
+    fout << "results1:" << std::endl;
+    fout << "{";
+    for (const double &result : results1)
+    {
+        fout << result << ", ";
+    }
+    fout << "}";
+    fout << std::endl;
+    fout << "results2:" << std::endl;
+    fout << "{";
+    for (const double &result : results2)
+    {
+        fout << result << ", ";
+    }
+    fout << "}";
+    fout << std::endl;
+    fout << "End" << std::endl;
+    fout << "------------------------" << std::endl;
+    fout.close();
+    //CHECK(EstimateFLA(200, 1780, Motor::LineFrequency::FREQ60, Motor::EfficiencyClass::SPECIFIED, 94, 460).calculate() == Approx(227.3));
+    //CHECK(EstimateFLA(200, 1780, Motor::LineFrequency::FREQ60, Motor::EfficiencyClass::SPECIFIED, 95, 460).calculate() == Approx(228.4));
 } 
