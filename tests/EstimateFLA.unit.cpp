ae49c6afc718d6df57418032d1361ef712984ae3
Colin Causey <causeyc@findlay.edu>
2020-03-24 11:57:37 -0400

    Added EstimateFLA unit test

---
+++ tests/EstimateFLA.unit.cpp
@@ -0,0 +1,9 @@
+#include "catch.hpp"
+#include <sqlite/SQLite.h>
+#include <calculator/motor/EstimateFLA.h>
+
+
+TEST_CASE( "Estimate FLA", "[EstimateFLA]" ) {
+ //CHECK(EstimateFLA(200, 1780, Motor::LineFrequency::FREQ60, Motor::EfficiencyClass::SPECIFIED, 94, 460).calculate() == Approx(227.3));
+ //CHECK(EstimateFLA(200, 1780, Motor::LineFrequency::FREQ60, Motor::EfficiencyClass::SPECIFIED, 95, 460).calculate() == Approx(228.4));
+} 
