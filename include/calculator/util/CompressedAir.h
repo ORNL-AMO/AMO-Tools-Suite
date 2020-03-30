0437b31836d502551e5dd5648ce7022f3b738991
Colin Causey <causeyc@findlay.edu>
2020-03-30 12:13:51 -0400

    Added 8, 10, 12, 16, 18, 20, 24 pipe to CompressedAir.h

--- include/calculator/util/CompressedAir.h
+++ include/calculator/util/CompressedAir.h
@@ -175,7 +175,8 @@ namespace Compressor {
 				  oneAndOneFourth(oneAndOneFourth * 0.0104), oneAndOneHalf(oneAndOneHalf * 0.0141),
 				  two(two * 0.0233), twoAndOneHalf(twoAndOneHalf * 0.0333), three(three * 0.0513),
 				  threeAndOneHalf(threeAndOneHalf * 0.0687), four(four * 0.0884), five(five * 0.1389),
-				  six(six * 0.2006),
+				  six(six * 0.2006), eight(eight * 0.3442), ten(0.5476), twelve(0.7763), fourteen(fourteen * 0.9354),
+				  sixteen(sixteen * 1.223), eighteen(eighteen * 1.555), twenty(twenty * 1.926), twentyFour(twentyFour * 2.793),
 				  totalPipeVolume(this->oneHalf + this->threeFourths + this->one + this->oneAndOneFourth
 				                  + this->oneAndOneHalf + this->two + this->twoAndOneHalf + this->three
 				                  + this->threeAndOneHalf + this->four + this->five + this->six)
@@ -192,11 +193,14 @@ namespace Compressor {
 				  oneAndOneFourth(compVel(1.5)), oneAndOneHalf(compVel(2.04)),
 				  two(compVel(3.36)), twoAndOneHalf(compVel(4.79)), three(compVel(7.39)),
 				  threeAndOneHalf(compVel(9.89)), four(compVel(12.73)), five(compVel(20)),
-				  six(compVel(28.89))
+				  six(compVel(28.89)), eight(compVel(50.02)), ten(compVel(78.85)), twelve(compVel(111.9)),
+				  fourteen(compVel(135.3)), sixteen(compVel(176.7)), eighteen(compVel(224)), twenty(compVel(278)),
+				  twentyFour(compVel(402.10))
 		{}
 
 		const double oneHalf, threeFourths, one, oneAndOneFourth, oneAndOneHalf, two;
 		const double twoAndOneHalf, three, threeAndOneHalf, four, five, six;
+		const double eight, ten, twelve, fourteen, sixteen, eighteen, twenty, twentyFour;
 		const double totalPipeVolume = 0;
 	};
 
