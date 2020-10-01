6a35af33617cd27bf6013d2386e75be24648c4a2
mark-root <mroot@intertech.com>
2020-10-01 11:09:35 -0500

    finish phast wasm bindings and add unit tests

--- bindings-wasm/phast/phast.cpp
+++ bindings-wasm/phast/phast.cpp
@@ -13,7 +13,15 @@
 #include "calculator/losses/LiquidCoolingLosses.h"
 #include "calculator/losses/LiquidLoadChargeMaterial.h"
 #include "calculator/losses/OpeningLosses.h"
-
+#include "calculator/losses/SlagOtherMaterialLosses.h"
+#include "calculator/losses/SolidLoadChargeMaterial.h"
+#include "calculator/losses/WallLosses.h"
+#include "calculator/losses/WaterCoolingLosses.h"
+#include "calculator/furnace/EfficiencyImprovement.h"
+#include "calculator/furnace/EnergyEquivalency.h"
+#include "calculator/furnace/FlowCalculationsEnergyUse.h"
+#include "calculator/furnace/O2Enrichment.h"
+#include "calculator/furnace/HumidityRatio.h"
 #include <emscripten/bind.h>
 using namespace emscripten;
 
@@ -147,12 +155,87 @@ EMSCRIPTEN_BINDINGS(openingLosses)
         .function("calculateViewFactor", &OpeningLosses::calculateViewFactor);
 }
 // slagOtherMaterialLosses
+EMSCRIPTEN_BINDINGS(slagOtherMaterialLosses)
+{
+    class_<SlagOtherMaterialLosses>("SlagOtherMaterialLosses")
+        .constructor<double, double, double, double, double>()
+        .function("getHeatLoss", &SlagOtherMaterialLosses::getHeatLoss);
+}
 // solidLoadChargeMaterial
+EMSCRIPTEN_BINDINGS(solidLoadChargeMaterial)
+{
+    class_<SolidLoadChargeMaterial>("SolidLoadChargeMaterial")
+        .constructor<LoadChargeMaterial::ThermicReactionType, double, double, double, double, double, double, double, double, double, double, double, double, double, double>()
+        .function("getTotalHeat", &SolidLoadChargeMaterial::getTotalHeat);
+}
 // wallLosses
+EMSCRIPTEN_BINDINGS(wallLosses)
+{
+    class_<WallLosses>("WallLosses")
+        .constructor<double, double, double, double, double, double, double>()
+        .function("getHeatLoss", &WallLosses::getHeatLoss);
+}
 // waterCoolingLosses
+EMSCRIPTEN_BINDINGS(waterCoolingLosses)
+{
+    class_<WaterCoolingLosses>("WaterCoolingLosses")
+        .constructor<double, double, double, double>()
+        .function("getHeatLoss", &WaterCoolingLosses::getHeatLoss);
+}
 // efficiencyImprovement
+EMSCRIPTEN_BINDINGS(efficiencyImprovement)
+{
+    class_<EfficiencyImprovement>("EfficiencyImprovement")
+        .constructor<double, double, double, double, double, double, double>()
+        .function("getCurrentExcessAir", &EfficiencyImprovement::getCurrentExcessAir)
+        .function("getNewExcessAir", &EfficiencyImprovement::getNewExcessAir)
+        .function("getCurrentAvailableHeat", &EfficiencyImprovement::getCurrentAvailableHeat)
+        .function("getNewAvailableHeat", &EfficiencyImprovement::getNewAvailableHeat)
+        .function("getNewFuelSavings", &EfficiencyImprovement::getNewFuelSavings)
+        .function("getNewEnergyInput", &EfficiencyImprovement::getNewEnergyInput);
+}
 // energyEquivalencyElectric
+EMSCRIPTEN_BINDINGS(energyEquivalencyElectric)
+{
+    class_<ElectricalEnergyEquivalency>("ElectricalEnergyEquivalency")
+        .constructor<double, double, double>()
+        .function("getElectricalHeatInput", &ElectricalEnergyEquivalency::getElectricalHeatInput);
+}
+
 // energyEquivalencyFuel
+EMSCRIPTEN_BINDINGS(energyEquivalencyFuel)
+{
+    class_<FuelFiredEnergyEquivalency>("FuelFiredEnergyEquivalency")
+        .constructor<double, double, double>()
+        .function("getFuelFiredHeatInput", &FuelFiredEnergyEquivalency::getFuelFiredHeatInput);
+}
+
 // flowCalculations
+EMSCRIPTEN_BINDINGS(flowCalculations)
+{
+    class_<FlowCalculationsEnergyUse>("FlowCalculationsEnergyUse")
+        .constructor<FlowCalculationsEnergyUse::Gas, double, double, double, FlowCalculationsEnergyUse::Section, double, double, double, double, double, double>()
+        .function("getFlow", &FlowCalculationsEnergyUse::getFlow)
+        .function("getHeatInput", &FlowCalculationsEnergyUse::getHeatInput)
+        .function("getTotalFlow", &FlowCalculationsEnergyUse::getTotalFlow);
+}
+
 // o2Enrichment
+EMSCRIPTEN_BINDINGS(o2Enrichment)
+{
+    class_<O2Enrichment>("O2Enrichment")
+        .constructor<double, double, double, double, double, double, double, double, double>()
+        .function("getAvailableHeat", &O2Enrichment::getAvailableHeat)
+        .function("getAvailableHeatEnriched", &O2Enrichment::getAvailableHeatEnriched)
+        .function("getFuelSavingsEnriched", &O2Enrichment::getFuelSavingsEnriched)
+        .function("getFuelConsumptionEnriched", &O2Enrichment::getFuelConsumptionEnriched);
+}
+
 // humidityRatio
+EMSCRIPTEN_BINDINGS(humidityRatio)
+{
+    class_<HumidityRatio>("HumidityRatio")
+        .constructor<double, double, double, double>()
+        .function("getHumidityRatioUsingRH", &HumidityRatio::getHumidityRatioUsingRH)
+        .function("getHumidityRatioUsingWBT", &HumidityRatio::getHumidityRatioUsingWBT);
+}
--- bindings-wasm/phast/phastEnum.cpp
+++ bindings-wasm/phast/phastEnum.cpp
@@ -1,4 +1,5 @@
 #include "calculator/losses/LoadChargeMaterial.h"
+#include "calculator/furnace/EnergyEquivalency.h"
 #include <emscripten/bind.h>
 using namespace emscripten;
 
@@ -8,4 +9,25 @@ EMSCRIPTEN_BINDINGS(phast_enums)
         .value("ENDOTHERMIC", LoadChargeMaterial::ThermicReactionType::ENDOTHERMIC)
         .value("EXOTHERMIC", LoadChargeMaterial::ThermicReactionType::EXOTHERMIC)
         .value("NONE", LoadChargeMaterial::ThermicReactionType::NONE);
+
+    enum_<FlowCalculationsEnergyUse::Gas>("Gas")
+        .value("AIR", FlowCalculationsEnergyUse::Gas::AIR)
+        .value("AMMONIA_DISSOCIATED", FlowCalculationsEnergyUse::Gas::AMMONIA_DISSOCIATED)
+        .value("ARGON", FlowCalculationsEnergyUse::Gas::ARGON)
+        .value("BUTANE", FlowCalculationsEnergyUse::Gas::BUTANE)
+        .value("ENDOTHERMIC_AMMONIA", FlowCalculationsEnergyUse::Gas::ENDOTHERMIC_AMMONIA)
+        .value("EXOTHERMIC_CRACKED_LEAN", FlowCalculationsEnergyUse::Gas::EXOTHERMIC_CRACKED_LEAN)
+        .value("EXOTHERMIC_CRACKED_RICH", FlowCalculationsEnergyUse::Gas::EXOTHERMIC_CRACKED_RICH)
+        .value("HELIUM", FlowCalculationsEnergyUse::Gas::HELIUM)
+        .value("HYDROGEN", FlowCalculationsEnergyUse::Gas::HYDROGEN)
+        .value("NATURAL_GAS", LoadChaFlowCalculationsEnergyUsergeMaterial::Gas::NATURAL_GAS)
+        .value("NITROGEN", FlowCalculationsEnergyUse::Gas::NITROGEN)
+        .value("OXYGEN", FlowCalculationsEnergyUse::Gas::OXYGEN)
+        .value("PROPANE", FlowCalculationsEnergyUse::Gas::PROPANE)
+        .value("OTHER", FlowCalculationsEnergyUse::Gas::OTHER);
+        
+    enum_<FlowCalculationsEnergyUse::Section>("Section")
+        .value("SQUARE_EDGE", FlowCalculationsEnergyUse::Section::SQUARE_EDGE)
+        .value("SHARP_EDGE", FlowCalculationsEnergyUse::Section::SHARP_EDGE)
+        .value("VENTURI", FlowCalculationsEnergyUse::Section::VENTURI);
 }
