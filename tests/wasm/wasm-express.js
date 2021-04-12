52adb3dba83b67ce2f09280bb5d4d43e8f887fc7
Robert Root <rmroot@live.com>
2021-04-12 12:28:39 -0500

    async testing

--- tests/wasm/wasm-express.js
+++ tests/wasm/wasm-express.js
@@ -7,13 +7,13 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/wasm-express.html'));
 });
 app.use('/assets', express.static('bin'));
+app.use('/tests', express.static('tests/wasm/test-helpers/'));
 app.use('/tests', express.static('tests/wasm/psat/'));
 app.use('/tests', express.static('tests/wasm/motor/'));
 app.use('/tests', express.static('tests/wasm/fan/'));
 app.use('/tests', express.static('tests/wasm/phast/'));
 app.use('/tests', express.static('tests/wasm/standalone/'));
 app.use('/tests', express.static('tests/wasm/ssmt/'));
-app.use('/tests', express.static('tests/wasm/test-helpers/'));
 app.use('/jquery', express.static('node_modules/jquery/dist/'));
 app.use('/assert', express.static('node_modules/assert-plus'));
 
