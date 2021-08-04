519fccc781d4473f1d28fcc0b4c496eb56bb36e5
rmroot <mroot@intertech.com>
2021-08-04 14:19:06 -0500

    more curve fit fixes

--- bindings/compressorsCalc.h
+++ bindings/compressorsCalc.h
@@ -332,34 +332,22 @@ void compressorsLoadUnload(Compressors::CompressorType compressorType, Compresso
 void compressorsModulationWithUnload(Compressors::CompressorType compressorType, Compressors::ControlType controlType, Compressors::Stage stageType, Compressors::Lubricant lubricantType)
 {
     const double powerAtFullLoad = getDouble("powerAtFullLoad", inp);
-    std::cout << "powerAtFullLoad: " <<  powerAtFullLoad << std::endl;
     const double capacityAtFullLoad = getDouble("capacityAtFullLoad", inp);
-    std::cout << "capacityAtFullLoad: " <<  capacityAtFullLoad << std::endl;
     const double receiverVolume = getDouble("receiverVolume", inp);
-    std::cout << "receiverVolume: " <<  receiverVolume << std::endl;
 
     const int computeFrom = getInteger("computeFrom");
     const double computeFromVal = getDouble("computeFromVal", inp);
     const bool applyPressureInletCorrection = getBool("applyPressureInletCorrection");
 
     const double powerMax = getDouble("powerMax", inp);
-    std::cout << "powerMax: " <<  powerMax << std::endl;
     const double powerAtNolLoad = getDouble("powerAtNolLoad", inp);
-    std::cout << "powerAtNolLoad: " <<  powerAtNolLoad << std::endl;
     const double dischargePsiFullLoad = getDouble("dischargePsiFullLoad", inp);
-    std::cout << "dischargePsiFullLoad: " <<  dischargePsiFullLoad << std::endl;
     const double dischargePsiMax = getDouble("dischargePsiMax", inp);
-    std::cout << "dischargePsiMax: " <<  dischargePsiMax << std::endl;
     const double modulatingPsi = getDouble("modulatingPsi", inp);
-    std::cout << "modulatingPsi: " <<  modulatingPsi << std::endl;
     const double atmosphericPsi = getDouble("atmosphericPsi", inp);
-    std::cout << "atmosphericPsi: " <<  atmosphericPsi << std::endl;
     const double unloadPointCapacity = getDouble("unloadPointCapacity", inp);
-    std::cout << "unloadPointCapacity: " <<  unloadPointCapacity << std::endl;
     const double blowdownTime = getDouble("blowdownTime", inp);
-    std::cout << "blowdownTime: " <<  blowdownTime << std::endl;
     const double unloadSumpPressure = getDouble("unloadSumpPressure", inp);
-    std::cout << "unloadSumpPressure: " <<  unloadSumpPressure << std::endl;
 
     
     auto compMethod = Compressors_ModulationWithUnload(powerAtFullLoad, capacityAtFullLoad, receiverVolume, powerMax, powerAtNolLoad, dischargePsiFullLoad, dischargePsiMax, modulatingPsi, atmosphericPsi, unloadPointCapacity, controlType, blowdownTime, unloadSumpPressure);
