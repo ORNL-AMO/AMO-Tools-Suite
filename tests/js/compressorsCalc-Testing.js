23405b28c2a75c22e8206f86bc1fbabba851dbb8
rmroot <mroot@intertech.com>
2021-10-01 12:13:15 -0500

    fixes mod exp setting for variable displacement

--- tests/js/compressorsCalc-Testing.js
+++ tests/js/compressorsCalc-Testing.js
@@ -7,76 +7,74 @@ function rnd(value) {
     return Number(Math.round(value));
 }
 
-// //ID: 629 Lubricant inject screw load/unload
-//NOT CLOSE
-test('COMP ID: 629 Lubricant inject screw load/unload', function (t) {
+//ID: 728 Lubricant injected screw variable displacement
+// THESE ARE ALL CLOSE (PROBABLY WITHIN TOLERANCE..)
+test('COMP ID: 728 Lubricant injected screw variable displacement', function (t) {
     t.plan(8);
     var input = {
         adjustForDischargePressure: false,
         applyPressureInletCorrection: false,
         atmosphericPsi: 14.7,
         blowdownTime: 40,
-        capacityAtFullLoad: 535,
-        capacityAtMaxFullFlow: 531,
-        capacityAtUnload: undefined,
+        capacityAtFullLoad: 1390,
+        capacityAtMaxFullFlow: 1390,
+        capacityAtUnload: 556,
         compressorType: 1,
         computeFrom: 1,
         computeFromPFAmps: 0,
         computeFromPFVoltage: 0,
         computeFromVal: 0.995,
-        controlType: 0,
+        controlType: 5,
         dischargePsiFullLoad: 100,
-        dischargePsiMax: 110,
-        loadFactorUnloaded: 0.20608899297423888,
+        dischargePsiMax: 100,
+        loadFactorUnloaded: 0.205,
         lubricantType: 0,
-        modulatingPsi: null,
-        noLoadPowerFM: 0,
-        powerAtFullLoad: 85.4,
+        modulatingPsi: 17,
+        noLoadPowerFM: 0.57,
+        powerAtFullLoad: 219.2,
         powerAtFullLoadPercentage: 1,
-        powerAtNoLoad: 17.6,
-        powerAtNolLoad: 17.6,
-        powerAtUnload: undefined,
-        powerMax: 90.1,
-        powerMaxPercentage: 1.0550351288056206,
-        pressureAtUnload: undefined,
-        receiverVolume: 13.400145795431047,
+        powerAtNoLoad: 45,
+        powerAtNolLoad: 45,
+        powerAtUnload: 140,
+        powerMax: 219.2,
+        powerMaxPercentage: 1,
+        pressureAtUnload: 110,
+        receiverVolume: 13.4,
         stageType: 1,
-        unloadPointCapacity: 100,
+        unloadPointCapacity: 40,
         unloadSumpPressure: 15
     }
-
-
 
     input.computeFromVal = .09;
     //1 = % Airflow
     input.computeFrom = 1;
     var results = bindings.CompressorsCalc(input);
     results.percentagePower = results.percentagePower * 100;
-    t.equal(rnd(results.percentagePower), rnd(71), "% Power 1");
-    t.equal(rnd(results.powerCalculated), rnd(60), "Power Calculated 1");
+    t.equal(rnd(results.percentagePower), rnd(57), "% Power 1");
+    t.equal(rnd(results.powerCalculated), rnd(125), "Power Calculated 1");
 
-    input.computeFromVal = .72;
+    input.computeFromVal = .57;
     //0 = Percent Power
     input.computeFrom = 0;
     var results = bindings.CompressorsCalc(input);
     results.percentageCapacity = results.percentageCapacity * 100;
     t.equal(rnd(results.percentageCapacity), rnd(10), "% Capacity 1");
-    t.equal(rnd(results.powerCalculated), rnd(61), "Power Calculated 2");
+    t.equal(rnd(results.powerCalculated), rnd(125), "Power Calculated 2");
 
 
-    input.computeFromVal = .91;
+    input.computeFromVal = .92;
     //1 = % Airflow
     input.computeFrom = 1;
     var results = bindings.CompressorsCalc(input);
     results.percentagePower = results.percentagePower * 100;
-    t.equal(rnd(results.percentagePower), rnd(101.4), "% Power 2");
-    t.equal(rnd(results.powerCalculated), rnd(86.54), "Power Calculated 3");
+    t.equal(rnd(results.percentagePower), rnd(93), "% Power 2");
+    t.equal(rnd(results.powerCalculated), rnd(204.72), "Power Calculated 3");
 
-    input.computeFromVal = 1.014;
+    input.computeFromVal = .937;
     //0 = Percent Power
     input.computeFrom = 0;
     var results = bindings.CompressorsCalc(input);
     results.percentageCapacity = results.percentageCapacity * 100;
-    t.equal(rnd(results.percentageCapacity), rnd(91), "% Capacity 2");
-    t.equal(rnd(results.powerCalculated), rnd(86.54), "Power Calaculated 4");
+    t.equal(rnd(results.percentageCapacity), rnd(92), "% Capacity 2");
+    t.equal(rnd(results.powerCalculated), rnd(204.72), "Power Calculated 4");
 });
