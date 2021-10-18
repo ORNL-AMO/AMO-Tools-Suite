1042b7a227209c66fb50612ecda1124a57753de1
omerb <omerb@ornl.gov>
2021-10-18 18:27:36 -0400

    undo overwrite

--- tests/js/lossesTest.js
+++ tests/js/lossesTest.js
@@ -89,7 +89,7 @@ test('flueGasByMass', function (t) {
 
     var inp = {
         flueGasTemperature: 700, excessAirPercentage: 9.0, combustionAirTemperature: 125, fuelTemperature: 70,
-        moistureInAirComposition: 1.0, ashDischargeTemperature: 100, unburnedCarbonInAsh: 1.5,
+        moistureInAirCombustion: 1.0, ashDischargeTemperature: 100, unburnedCarbonInAsh: 1.5,
         carbon: 75.0, hydrogen: 5.0, sulphur: 1.0, inertAsh: 9.0, o2: 7.0, moisture: 0.0, nitrogen: 1.5, ambientAirTempF:60
     };
 
